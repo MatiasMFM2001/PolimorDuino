b5c570578da94733967c43091d1bcb33117c529e
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-30 20:20:46 -0300

    Documentación inicial de `Logger/CLASE_BufferDatos.h`

--- Logger/CLASE_BufferDatos.h
+++ Logger/CLASE_BufferDatos.h
@@ -3,19 +3,41 @@
 
 #include <Print.h>
 #include <Array.h>
-    template <size_t TNumBytes>
-    
+	/**
+	 * @brief Permite almacenar temporalmente los bytes que serán impresos por
+	 *  una instancia de @code Print, cuando ésta requiere tener las
+	 *  interrupciones activadas para funcionar, pero fueron desactivadas
+	 *  temporalmente.
+	 * 
+	 * @tparam TNumBytes La capacidad del buffer, en bytes.
+	 */
+    template <size_t TNumBytes> 
     class BufferDatos: public Print {
         private:
+            /** @brief Estructura que almacena los bytes del buffer. */
             Array<byte, TNumBytes> datos;
+            
+            /** @brief Instancia que imprimirá los bytes del buffer. */
             Print* salida;
         
         public:
+            /**
+             * @brief Construye un BufferDatos, con el array vacío y la
+             *  impresora especificada.
+             * 
+             * @param salida La impresora especificada (que puede ser nula).
+             */
             BufferDatos(Print* salida = nullptr)
                 : Print()
                 , salida(salida)
             {}
         
+            /**
+             * @brief Escribe un byte en el buffer, si hay capacidad suficiente.
+             * 
+             * @param ingr El byte a escribir.
+             * @returns 1 si hay capacidad, 0 en caso contrario.
+             */
             size_t write(byte ingr) override {
                 if (this -> datos.full()) {
                     return 0;
@@ -25,6 +47,10 @@
                 return 1;
             }
             
+            /**
+             * @brief Si la impresora no es nula, le envía todos los datos
+             *  almacenados y vacía el buffer.
+             */
             void flush() override {
                 if (this -> salida == nullptr) {
                     return;
