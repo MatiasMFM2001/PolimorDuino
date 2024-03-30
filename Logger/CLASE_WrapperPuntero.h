f80beddddd5e2623b1d637fa939ee803c3a20a5c
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-30 20:45:31 -0300

    Documentación inicial de `Logger/CLASE_WrapperPuntero.h`

--- Logger/CLASE_WrapperPuntero.h
+++ Logger/CLASE_WrapperPuntero.h
@@ -2,41 +2,70 @@
 #define WRAPPER_PUNTERO
 
 #include <Printable.h>
-#include "FuncionesJSON.h"
+#define NULLPTR_TO_STRING "null"
+    /**
+     * @brief Encapsula un puntero crudo, permitiendo imprimirlo.
+     * 
+     * @tparam TTipoDato El tipo de dato a apuntar.
+     */
     template <typename TTipoDato>
     class WrapperPuntero : public Printable {
         private:
+            /** @brief Puntero al dato. */
             TTipoDato* puntero;
         
         public:
+            /**
+             * @brief Construye un WrapperPuntero, con el puntero especificado.
+             * 
+             * @param puntero El puntero especificado.
+             */
             WrapperPuntero(TTipoDato* puntero = nullptr)
                 : puntero(puntero)
             {}
             
             
-            
+            /**
+             * @returns El puntero encapsulado.
+             */
             TTipoDato* getPuntero() const {
                 return (this -> puntero);
             }
             
+            /**
+             * @returns La referencia al dato apuntado por el puntero
+             *  encapsulado.
+             */
             TTipoDato& getDato() const {
                 return *(this -> puntero);
             }
             
+            /**
+             * @returns @code true si el puntero es nulo, @code false en caso
+             *  contrario.
+             */
             bool esNulo() const {
                 return ((this -> puntero) == nullptr);
             }
         
+            /**
+             * @brief Imprime el dato apuntado a la impresora especificada.
+             * 
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
             size_t printTo(Print& impresora) const override {
                 if (this -> esNulo()) {
-                    return impresora.print(JSON_NULL);
+                    return impresora.print(NULLPTR_TO_STRING);
                 }
                 
                 return impresora.print(this -> getDato());
             }
             
             
-            
+            /**
+             * @returns El puntero encapsulado.
+             */
             operator TTipoDato*() const {
                 return (this -> puntero);
             }
