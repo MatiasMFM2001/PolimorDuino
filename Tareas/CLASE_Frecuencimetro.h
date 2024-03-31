41edf0b4b2cc981efe60adffd2a39bfd0ac1661e
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-30 23:55:25 -0300

    Documentación inicial de `Tareas/CLASE_Frecuencimetro.h`

--- Tareas/CLASE_Frecuencimetro.h
+++ Tareas/CLASE_Frecuencimetro.h
@@ -3,20 +3,55 @@
 
 #include "InclusionTaskSchedulerDeclarations.h"
 #include "CLASE_Contador.h"
+	/**
+     * @brief Tarea que permite medir la frecuencia de una forma de onda
+     *  periódica, a partir de la cantidad de flancos por segundo.
+     */
     class Frecuencimetro : public Task {
         private:
+			/** @brief Contador de flancos transitados. */
             Contador<unsigned short> contFrec;
+            
+            /** @brief Último valor de frecuencia medido. */
             unsigned short ultFrec;
     
         public:
+            /**
+             * @brief Construye un Frecuencimetro, con el scheduler especificado.
+             * 
+             * @param planif El planificador de tareas especificado (que puede
+             *  ser nulo).
+             */
             Frecuencimetro(Scheduler* planif);
-    
+
+            /**
+             * @brief De forma periódica, finaliza la medición actual e inicia
+             *  una nueva.
+             * 
+             * @returns @code true para indicar que la ejecución de la tarea fue
+             *  "productiva".
+             */
             bool Callback() override;
-            
+
+            /**
+             * @brief Reinicia el contador de flancos transitados.
+             */
             void reiniciar();
-            void incrementar();
             
+            /**
+             * @brief Incrementa en 1 el contador de flancos transitados.
+             */
+            void incrementar();
+
+            /**
+             * @returns El valor de la última medición realizada, en Hz.
+             */
             unsigned short getHz();
+            
+            /**
+             * @returns @code true si el contador vale 0, @code false en caso
+             *  contrario.
+             */
             bool estaEnCero();
     };
 #endif
