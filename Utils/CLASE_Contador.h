0907e26bdd6c644e5e66b3048414d5c0daea0177
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-31 02:51:08 -0300

    Documentación inicial de `Utils/CLASE_Contador.h`

--- Utils/CLASE_Contador.h
+++ Utils/CLASE_Contador.h
@@ -2,37 +2,80 @@
 #define CONTADOR_T
 
 #include "FuncionesJSON.h"
+    /**
+     * @brief Encapsula una variable a usar como contador, permitiendo
+     *  imprimirla.
+     * 
+     * @tparam T El tipo de dato a usar para almacenar los valores del contador.
+     */
     template <typename T>
-    
     class Contador : public Printable {
         private:
-            T valorInicial, valorActual;
+            /** @brief Valor inicial del contador. */
+            T valorInicial;
+            
+            /** @brief Valor final del contador. */
+            T valorActual;
     
         public:
+            /**
+             * @brief Construye un Contador, con el valor inicial especificado.
+             * 
+             * @param valorInicial El valor inicial especificado.
+             */
             Contador(T valorInicial)
                 : valorInicial(valorInicial), valorActual(valorInicial)
             {}
     
+            /**
+             * @brief Incrementa el valor actual del contador, según la cantidad
+             *  especificada.
+             * 
+             * @param cantidad La cantidad especificada.
+             */
             void incrementar(T cantidad) {
                 (this -> valorActual) += cantidad;
             }
             
+            /**
+             * @brief Decrementa el valor actual del contador, según la cantidad
+             *  especificada.
+             * 
+             * @param cantidad La cantidad especificada.
+             */
             void decrementar(T cantidad) {
                 (this -> valorActual) -= cantidad;
             }
     
+            /**
+             * @returns El valor actual.
+             */
             T getValor() {
                 return (this -> valorActual);
             }
             
+            /**
+             * @brief Regresa el contador a su valor inicial.
+             */
             void reiniciar() {
                 this -> valorActual = this -> valorInicial;
             }
             
+            /**
+             * @returns @code true si el valor actual del contador es igual al
+             *  inicial, @code false en caso contrario.
+             */
             bool estaReiniciado() {
                 return (this -> valorActual == this -> valorInicial);
             }
             
+            /**
+             * @brief Imprime los valores de las variables de instancia a la
+             *  impresora especificada.
+             * 
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
             size_t printTo(Print& impresora) const override {
                 return (imprimirCabeceraJSON(impresora, F("Contador"))
                     + imprimirVariableJSON(impresora, F("valorInicial"), this -> valorInicial) + impresora.print(JSON_SEPARADOR)
