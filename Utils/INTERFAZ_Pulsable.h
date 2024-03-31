aa3de159fec3006a93c6b718fc88019006cd09cf
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-31 03:25:52 -0300

    Documentación inicial de `Utils/INTERFAZ_Pulsable.h`

--- Utils/INTERFAZ_Pulsable.h
+++ Utils/INTERFAZ_Pulsable.h
@@ -2,9 +2,20 @@
 #define PULSABLE
 
 #include <Printable.h>
+	/**
+	 * @brief Define una interfaz para los objetos que puedan ser encendidos y
+	 *  apagados.
+	 */
     class Pulsable : virtual public Printable {
         public:
+            /**
+             * @brief Pone al objeto en estado "Encendido".
+             */
             virtual void encender() = 0;
+            
+            /**
+             * @brief Pone al objeto en estado "Apagado".
+             */
             virtual void apagar() = 0;
     };
 #endif
