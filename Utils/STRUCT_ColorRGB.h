57fb206416b26b862be14901f47af1890741e59e
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-01 00:34:08 -0300

    Documentación inicial de `Utils/STRUCT_ColorRGB.h`

--- Utils/STRUCT_ColorRGB.h
+++ Utils/STRUCT_ColorRGB.h
@@ -2,10 +2,16 @@
 #define COLOR_RGB
 
 #include "TIPO_Byte.h"
+	/**
+	 * @brief Almacena los brillos de los subpíxeles de un color RGB.
+	 */
     struct ColorRGB {
         byte rojo, verde, azul;
     };
 
+	/**
+	 * @brief Almacena los brillos de los subpíxeles de un color RGBW.
+	 */
     struct ColorRGBW : ColorRGB {
         byte blanco;
     };
