d5d95a58f2dfd91139647d663cedca8b5b88da89
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-01 00:36:21 -0300

    Unificar `ColorRGB` en `ColorRGBW`

---
+++ Utils/STRUCT_ColorRGBW.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_RGBW
+#define COLOR_RGBW
+
+#include "TIPO_Byte.h"
+	/**
+	 * @brief Almacena los brillos de los subpíxeles de un color RGBW.
+	 */
+    struct ColorRGBW {
+		/** @brief Brillo del subpíxel rojo. */
+        byte rojo;
+        
+        /** @brief Brillo del subpíxel verde. */
+        byte verde
+        
+        /** @brief Brillo del subpíxel azul. */
+        byte azul;
+        
+        /** @brief Brillo del subpíxel blanco. */
+        byte blanco = 0;
+    };
+#endif
