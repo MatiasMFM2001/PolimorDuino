49feaf1215d1ee5560657ff35a72cc795c33f291
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-31 22:10:50 -0300

    Documentación inicial de `Utils/CLASE_Medidor.h`

--- Utils/CLASE_Medidor.h
+++ Utils/CLASE_Medidor.h
@@ -2,10 +2,21 @@
 #define MEDIDOR
 
 #include "InclusionLog4Arduino.h"
+    /**
+     * @brief Define una interfaz para los objetos que puedan medir parámetros,
+     *  realizando acciones con los valores medidos.
+     * 
+     * @tparam T El tipo de dato que almacena el resultado de una medición.
+     */
     template <typename T>
-
     class Medidor {
         protected:
+            /**
+             * @brief Usa el valor medido para imprimirlo en el log, y ejecutar
+             *  el @code Callback().
+             * 
+             * @param ingr El valor medido.
+             */
             void ejecutarCallback(T ingr) {
                 FLOGS("MEDIDOR - Ejecutando ejecutarCallback()");
                 LOGS(ingr);
@@ -13,6 +24,11 @@
             }
         
         public:
+            /**
+             * @brief Realiza acciones con el valor medido.
+             * 
+             * @param valor El valor medido.
+             */
             virtual void Callback(T valor) {
                 FLOGS("MEDIDOR - Ejecutando Callback()");
                 LOGS(valor);
