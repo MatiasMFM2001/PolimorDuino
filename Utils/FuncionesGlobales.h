4bae65bab667d50d28e46f0f1be83f8a367a1c56
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-01 00:21:27 -0300

    Documentación inicial de `Utils/FuncionesGlobales.h`

--- Utils/FuncionesGlobales.h
+++ Utils/FuncionesGlobales.h
@@ -6,13 +6,62 @@
 #include "InclusionLog4Arduino.h"
 #include <Arduino.h>
 #include "DeclaracionVariablesGlobales.h"
+	/**
+	 * @brief Determina si el valor ingresado está dentro de los límites
+	 *  (inclusivos) especificados.
+     * 
+     * @param valor El valor ingresado.
+     * @param min El valor mínimo del rango especificado.
+     * @param max El valor máximo del rango especificado.
+     * @returns @code true si el valor está dentro de los límites, @code false
+     *  en caso contrario.
+     */
     bool enRango(short valor, short min, short max);
-  	short limitarRango(short valor, short min, short max);
-  	bool iguales(short num1, short num2, short difMax);
-  	//Scheduler* getNivel(Prioridad ingr);
-  	bool esPar (short num);
-    long multiplicarNumIteraciones(long numTerminos, unsigned long multiplicador);
     
+    /**
+	 * @brief Se asegura que el valor ingresado esté dentro de los límites
+	 *  (inclusivos) especificados.
+     * 
+     * @param valor El valor ingresado.
+     * @param min El valor mínimo del rango especificado.
+     * @param max El valor máximo del rango especificado.
+     * @returns El valor ingresado si está dentro de los límites, el valor
+     *  mínimo si éste es mayor al ingreado, o el valor máximo si éste es menor
+     *  al ingresado.
+     */
+  	short limitarRango(short valor, short min, short max);
+    
+    /**
+	 * @brief Determina si dos números son cercanos entre sí, teniendo en cuenta
+     *  el valor de tolerancia máximo.
+     * 
+     * @param num1 El primer número a comparar.
+     * @param num2 El segundo número a comparar.
+     * @param max La tolerancia/distancia máxima a contemplar.
+     * @returns @code true si la diferencia entre los números es menor o igual a
+     *  la tolerancia máxima, @code false en caso contrario.
+     */
+  	bool iguales(short num1, short num2, short difMax);
+  	
+    /**
+	 * @brief Determina si el número ingresado es par.
+     * 
+     * @param num El número a verificar.
+     * @returns @code true si el número es par, @code false en caso contrario.
+     */
+  	bool esPar(short num);
+    
+    /**
+	 * @brief Permite multiplicar una cantidad de iteraciones de una tarea (si
+     *  es distinta a @code TASK_FOREVER) por otra cantidad.
+     * 
+     * @param numTerminos La cantidad de iteraciones de la tarea.
+     * @param multiplicador La cantidad que multiplicará al número anterior.
+     * @returns @code TASK_FOREVER si la cantidad de iteraciones es igual a
+     *  @code TASK_FOREVER, el resultado de la multiplicación en caso contrario.
+     */
+    long multiplicarNumIteraciones(long numTerminos, unsigned long multiplicador);
+
     void notificarAlarma();
     void activarAlarma();
     void desactivarAlarma();
