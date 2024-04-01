1a4f828c1eefa8c65b412c68307d4e83ccad192d
Matías Manzin <matias.manzin@outlook.com.ar>
2024-03-31 22:13:24 -0300

    Eliminar `Utils/ENUM_ListaLEDs.h`

--- Utils/ENUM_ListaLEDs.h
+++
@@ -1,8 +0,0 @@
-#ifndef LISTA_LEDS
-#define LISTA_LEDS
-    enum ListaLEDs {
-        ESTADO,
-        
-        SIZE_LISTALEDS    // Como el valor es autoincremental, este valor termina siendo la cantidad de elementos
-    };
-#endif
