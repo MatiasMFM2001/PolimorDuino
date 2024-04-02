c32b46643add1d97b25c6a825408de79be8d3129
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-02 16:57:47 -0300

    Crear e implementar clase `MedidorInstantaneo`

---
+++ Medidores/Instantaneos/CLASE_MedidorInstantaneo.h
@@ -0,0 +1,21 @@
+#ifndef MEDIDOR_INSTANTANEO
+#define MEDIDOR_INSTANTANEO
+
+#include "CLASE_Medidor.h"
+    /**
+     * @brief 
+     * 
+     * @tparam TResultado El tipo de dato que almacena el resultado de una
+     *  medición.
+     */
+    template <typename TResultado>
+    class MedidorInstantaneo : public Medidor {
+        public:
+            MedidorInstantaneo(const __FlashStringHelper *nombre)
+                : Medidor(nombre)
+            {}
+        
+            virtual void iniciarMedicion() override {
+            }
+    };
+#endif
