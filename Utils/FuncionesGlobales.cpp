b3d8504b62addcd38b231156c4571e5a2179a19c
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-01 21:58:26 -0300

    Hacer genéricas a las funciones globales `enRango()`, `limitarRango()`, `iguales()`, `esPar()`

--- Utils/FuncionesGlobales.cpp
+++ Utils/FuncionesGlobales.cpp
@@ -1,24 +1,6 @@
 //#include "DeclaracionVariablesGlobales.h"
 #include "FuncionesGlobales.h"
 
-bool enRango(short valor, short min, short max) {
-    return (valor >= min) && (valor <= max);
-}
-
-short limitarRango(short valor, short min, short max) {
-    LOG("EJECUTANDO FuncionesGlobales::limitarRango(%d, %d, %d)", valor, min, max);
-    return constrain(valor, min, max);
-}
-
-bool iguales(short num1, short num2, short difMax) {
-    short dif = (num1 - num2);
-    return (abs(dif) < difMax);
-}
-
-bool esPar(short num) {
-    return (num % 2) == 0;
-}
-
 long multiplicarNumIteraciones(long numTerminos, unsigned long multiplicador) {
     if (numTerminos == TASK_FOREVER) {
         return TASK_FOREVER;
--- Utils/FuncionesGlobales.h
+++ Utils/FuncionesGlobales.h
@@ -10,18 +10,23 @@
 	 * @brief Determina si el valor ingresado está dentro de los límites
 	 *  (inclusivos) especificados.
      * 
+     * @tparam T El tipo de dato de los valores a procesar.
      * @param valor El valor ingresado.
      * @param min El valor mínimo del rango especificado.
      * @param max El valor máximo del rango especificado.
      * @returns @code true si el valor está dentro de los límites, @code false
      *  en caso contrario.
      */
-    bool enRango(short valor, short min, short max);
+    template <typename T>
+    bool enRango(T valor, T min, T max) {
+        return (valor >= min) && (valor <= max);
+    }
     
     /**
 	 * @brief Se asegura que el valor ingresado esté dentro de los límites
 	 *  (inclusivos) especificados.
      * 
+     * @tparam T El tipo de dato de los valores a procesar y retornar.
      * @param valor El valor ingresado.
      * @param min El valor mínimo del rango especificado.
      * @param max El valor máximo del rango especificado.
@@ -29,27 +34,39 @@
      *  mínimo si éste es mayor al ingreado, o el valor máximo si éste es menor
      *  al ingresado.
      */
-  	short limitarRango(short valor, short min, short max);
+    template <typename T>
+  	T limitarRango(T valor, T min, T max) {
+        return constrain(valor, min, max);
+    }
     
     /**
 	 * @brief Determina si dos números son cercanos entre sí, teniendo en cuenta
      *  el valor de tolerancia máximo.
      * 
+     * @tparam T El tipo de dato de los valores a procesar.
      * @param num1 El primer número a comparar.
      * @param num2 El segundo número a comparar.
      * @param max La tolerancia/distancia máxima a contemplar.
      * @returns @code true si la diferencia entre los números es menor o igual a
      *  la tolerancia máxima, @code false en caso contrario.
      */
-  	bool iguales(short num1, short num2, short difMax);
+    template <typename T>
+  	bool iguales(T num1, T num2, T difMax) {
+        T dif = (num1 - num2);
+        return (abs(dif) < difMax);
+    }
   	
     /**
 	 * @brief Determina si el número ingresado es par.
      * 
+     * @tparam T El tipo de dato del valor a procesar.
      * @param num El número a verificar.
      * @returns @code true si el número es par, @code false en caso contrario.
      */
-  	bool esPar(short num);
+  	template <typename T>
+    bool esPar(T num) {
+        return (num % 2) == 0;
+    }
     
     /**
 	 * @brief Permite multiplicar una cantidad de iteraciones de una tarea (si
