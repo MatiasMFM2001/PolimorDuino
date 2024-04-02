1aa7d7cdc0d8326d381966688e69155e22c4fa58
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-02 17:11:12 -0300

    Crear e implementar clase `MedidorTemporizado`

---
+++ Medidores/Temporizados/CLASE_MedidorTemporizado.h
@@ -0,0 +1,21 @@
+#ifndef MEDIDOR_TEMPORIZADO
+#define MEDIDOR_TEMPORIZADO
+
+#include "CLASE_Medidor.h"
+    /**
+     * @brief 
+     * 
+     * @tparam TResultado El tipo de dato que almacena el resultado de una
+     *  medición.
+     */
+    template <typename TResultado>
+    class MedidorTemporizado : public Medidor {
+        protected:
+            TResultado resultado;
+        
+        public:
+            MedidorInstantaneo(const __FlashStringHelper *nombre)
+                : Medidor(nombre)
+            {}
+    };
+#endif
