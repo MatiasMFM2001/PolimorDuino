3ad55efee9dd901cbc432494daa287be40d1649d
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-02 16:53:03 -0300

    Simplificar `Medidor`

--- Medidores/CLASE_Medidor.h
+++ Medidores/CLASE_Medidor.h
@@ -8,25 +8,11 @@
      * 
      * @tparam TResultado El tipo de dato que almacena el resultado de una
      *  medición.
-     * @tparam FLogger La función encargada de imprimir en logs el valor medido.
      */
-    template <typename TResultado, void (*FLogger)(TResultado&)>
+    template <typename TResultado>
     class Medidor {
         protected:
             const __FlashStringHelper *nombre;
-            //TResultado resultado;
-        
-            /**
-             * @brief Usa el valor medido para imprimirlo en el log, y ejecutar
-             *  el @code Callback().
-             * 
-             * @param ingr El valor medido.
-             */
-            void ejecutarCallback(TResultado ingr) {
-                LOG("MEDIDOR \"%S\" - Ejecutando Callback() con valor:", this -> nombre);
-                FLogger(ingr);
-                this -> Callback(ingr);
-            }
         
         public:
             Medidor(const __FlashStringHelper *nombre)
@@ -34,12 +20,6 @@
             {}
         
             virtual void iniciarMedicion() = 0;
-        
-            /**
-             * @brief Realiza acciones con el valor medido.
-             * 
-             * @param valor El valor medido.
-             */
-            virtual void Callback(TResultado valor) = 0;
+            virtual void finalizarMedicion() = 0;
     };
 #endif
