fcb9e22da1ff034524ff0c4220414f5af47cabe2
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-02 18:06:38 -0300

    Hacer que `Medidor` tenga una instancia de `CallbackResultado`

--- Medidores/CLASE_Medidor.h
+++ Medidores/CLASE_Medidor.h
@@ -8,15 +8,31 @@
      * 
      * @tparam TResultado El tipo de dato que almacena el resultado de una
      *  medición.
+     * @tparam FLogger La función encargada de imprimir en logs el valor medido.
      */
-    template <typename TResultado>
+    template <typename TResultado, void (*FLogger)(TResultado&)>
     class Medidor {
+        private:
+            CallbackResultado<TResultado> *callback;
+            
         protected:
             const __FlashStringHelper *nombre;
         
+            /**
+             * @brief Usa el valor medido para imprimirlo en el log, y ejecutar
+             *  el @code callback.
+             * 
+             * @param ingr El valor medido.
+             */
+            void ejecutarCallback(TResultado ingr) {
+                LOG("MEDIDOR \"%S\" - Ejecutando callback.notificar() con valor:", this -> nombre);
+                FLogger(ingr);
+                this -> callback -> notificar(ingr);
+            }
+        
         public:
-            Medidor(const __FlashStringHelper *nombre)
-                : nombre(nombre)
+            Medidor(const __FlashStringHelper *nombre, CallbackResultado<TResultado> *callback)
+                : nombre(nombre), callback(callback)
             {}
         
             virtual void iniciarMedicion() = 0;
