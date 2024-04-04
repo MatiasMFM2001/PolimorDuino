08fe05f94c6e9ebb5d7b612148a633c2e8e4298a
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-04 02:42:54 -0300

    Crear e implementar clase `PinSalidaPWM`

---
+++ EntradaSalida/Pines/CLASE_PinSalidaPWM.h
@@ -0,0 +1,28 @@
+#ifndef PIN_SALIDA_PWM
+#define PIN_SALIDA_PWM
+
+#include "CLASE_Pin.h"
+#include <PinChangeInterrupt.h>
+	/**
+	 * @brief Permite encapsular en un objeto, un pin de Salida PWM.
+	 */
+    template <typename T, byte NumBits>
+    class PinSalidaPWM : public Pin, public SalidaPWM<T, NumBits> {
+        public:
+			/**
+             * @brief Construye un PinSalidaPWM, con el número de pin,
+             *  configuración de inversión y estado inicial especificados.
+             * 
+             * @param numPin El número de pin especificado.
+             * @param invertir @code true para invertir el funcionamiento,
+             *  @code false para el caso contrario.
+             */
+            PinSalidaPWM(byte numPin = -1, bool invertir = false);
+        
+            void escribirBajoNivel(T valor) override {
+                if (this -> pinValido) {
+                    analogWrite(this -> pin);
+                }
+            }
+    };
+#endif
