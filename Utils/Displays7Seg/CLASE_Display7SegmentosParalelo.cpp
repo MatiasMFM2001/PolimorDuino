6625dbe8de5275ef36f865b1bbe69a18dd20449b
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-04 03:47:35 -0300

    Renombrar clase `Display7Segmentos` a `Display7SegmentosParalelo`

--- Utils/Displays7Seg/CLASE_Display7SegmentosParalelo.cpp
+++ Utils/Displays7Seg/CLASE_Display7SegmentosParalelo.cpp
@@ -1,8 +1,8 @@
-#include "CLASE_Display7Segmentos.h"
+#include "CLASE_Display7SegmentosParalelo.h"
 #include "InclusionLog4Arduino.h"
 #include "FuncionesJSON.h"
 
-Display7Segmentos::Display7Segmentos(Array<byte, NUM_PINES_DISPLAY> pinesDatos, byte pinEnable, bool invertirDatos, bool invertirEnable)
+Display7SegmentosParalelo::Display7SegmentosParalelo(Array<byte, NUM_PINES_DISPLAY> pinesDatos, byte pinEnable, bool invertirDatos, bool invertirEnable)
     : pinEnable(PinSalida(pinEnable, invertirEnable))
 {
     for (byte selec: pinesDatos) {
@@ -10,27 +10,27 @@ Display7Segmentos::Display7Segmentos(Array<byte, NUM_PINES_DISPLAY> pinesDatos, 
     }
 }
 
-void Display7Segmentos::setNumero(byte valor) {
-    LOG("INICIO Display7Segmentos::setNumero(%d)", valor);
+void Display7SegmentosParalelo::setNumero(byte valor) {
+    LOG("INICIO Display7SegmentosParalelo::setNumero(%d)", valor);
         byte bits = this -> getBits(valor);
         
         for (PinSalida selec: this -> pinesDatos) {
             selec.setEstado(bits & 0b00000001);
             bits >>= 1;
         }
-    LOG("FIN Display7Segmentos::setNumero(%#x)", valor);
+    LOG("FIN Display7SegmentosParalelo::setNumero(%#x)", valor);
 }
 
-void Display7Segmentos::encender(void) {
+void Display7SegmentosParalelo::encender(void) {
     this -> pinEnable.encender();
 }
 
-void Display7Segmentos::apagar(void) {
+void Display7SegmentosParalelo::apagar(void) {
     this -> pinEnable.apagar();
 }
 
-size_t Display7Segmentos::printTo(Print& impresora) const {
-    return (imprimirCabeceraJSON(impresora, F("Display7Segmentos"))
+size_t Display7SegmentosParalelo::printTo(Print& impresora) const {
+    return (imprimirCabeceraJSON(impresora, F("Display7SegmentosParalelo"))
         + imprimirVariableJSON(impresora, F("pinesDatos"), this -> pinesDatos) + impresora.print(JSON_SEPARADOR)
         + imprimirVariableJSON(impresora, F("pinEnable"), this -> pinEnable) + impresora.print(JSON_CLAUSURA_OBJETO)
     );
--- Utils/Displays7Seg/CLASE_Display7SegmentosParalelo.h
+++ Utils/Displays7Seg/CLASE_Display7SegmentosParalelo.h
@@ -1,17 +1,17 @@
-#ifndef DISPLAY_7_SEGMENTOS
-#define DISPLAY_7_SEGMENTOS
+#ifndef DISPLAY_7_SEGMENTOS_PARALELO
+#define DISPLAY_7_SEGMENTOS_PARALELO
 
 #include <Array.h>
 #include "CLASE_PinSalida.h"
 #include "Constantes.h"
-    class Display7Segmentos : public Pulsable {
+    class Display7SegmentosParalelo : public Pulsable {
         private:
             Array<PinSalida, NUM_PINES_DISPLAY> pinesDatos;
             PinSalida pinEnable;
             //byte numero;
         
         public:
-            Display7Segmentos(Array<byte, NUM_PINES_DISPLAY> pinesDatos, byte pinEnable, bool invertirDatos, bool invertirEnable);
+            Display7SegmentosParalelo(Array<byte, NUM_PINES_DISPLAY> pinesDatos, byte pinEnable, bool invertirDatos, bool invertirEnable);
         
             virtual byte getBits(byte valor) = 0;
             void setNumero(byte valor);
