0c8e1dcb204a8248b2bdc155c5659317bd6a042f
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-04 03:25:15 -0300

    Convertir `LectorADC` a `LectorEntrada`

---
+++ Medidores/Instantaneos/CLASE_LectorEntrada.h
@@ -0,0 +1,21 @@
+#ifndef LECTOR_Entrada
+#define LECTOR_Entrada
+
+
+#include "CLASE_MedidorInstantaneo.h"
+    template <typename TResultado, byte NumBits, void (*FLogger)(TResultado&)>
+    class LectorEntrada : MedidorInstantaneo<TResultado, FLogger> {
+        private:
+            Entrada<TResultado, NumBits> *entrada;
+      
+        public:
+            LectorEntrada(const __FlashStringHelper *nombre, CallbackResultado<TResultado> *callback, Scheduler* planif, Entrada<TResultado, NumBits> *entrada)
+                : MedidorInstantaneo(nombre, callback, planif)
+                , entrada(entrada)
+            {}
+            
+            virtual TResultado getResultado(void) override {
+                return this -> entrada -> leer();
+            }
+    };
+#endif
