3be9dd9afeae6e76942419ff0360b4d1dc697d6d
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-04 03:09:49 -0300

    Crear archivo de `TIPOS_EnterosAnalogicos.h`

---
+++ EntradaSalida/TIPOS_EnterosAnalogicos.h
@@ -0,0 +1,8 @@
+#define BYTE_4_BIT byte, 4
+#define BYTE_6_BIT byte, 6
+#define BYTE_8_BIT byte, 8
+
+#define USHORT_10_BIT unsigned short, 10
+#define USHORT_12_BIT unsigned short, 12
+#define USHORT_14_BIT unsigned short, 14
+#define USHORT_16_BIT unsigned short, 16
