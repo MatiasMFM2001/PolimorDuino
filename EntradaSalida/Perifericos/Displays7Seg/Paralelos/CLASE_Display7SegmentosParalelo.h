e0dd9bd960e734d0874fad654874481c5e28e37d
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-05 00:39:07 -0300

    Corregir implementación de clase `Display7SegmentosParalelo`

--- EntradaSalida/Perifericos/Displays7Seg/Paralelos/CLASE_Display7SegmentosParalelo.h
+++ EntradaSalida/Perifericos/Displays7Seg/Paralelos/CLASE_Display7SegmentosParalelo.h
@@ -3,21 +3,41 @@
 
 #include <Array.h>
 #include "CLASE_Display7Segmentos.h"
-    template <byte NumPinesDatos>
+    template <size_t NumSalidasDatos>
     class Display7SegmentosParalelo : public Display7Segmentos<byte> {
         private:
-            Array<SalidaDigital*, NumPinesDatos> salidasDatos;
+            Array<SalidaDigital*, NumSalidasDatos> salidasDatos;
             SalidaDigital *salidaEnable;
         
         protected:
             virtual byte getBits(byte valor) = 0;
         
         public:
-            Display7SegmentosParalelo(Array<SalidaDigital*, NumPinesDatos> salidasDatos, SalidaDigital *salidaEnable);
+            Display7SegmentosParalelo(Array<SalidaDigital*, NumSalidasDatos> salidasDatos, SalidaDigital *salidaEnable)
+                : salidasDatos(salidasDatos)
+                , salidaEnable(salidaEnable)
+            {}
         
-            void setNumero(byte valor);
-            void setEstado(bool valor) override;
+            void setNumero(byte valor) override {
+                LOG("INICIO Display7SegmentosParalelo::setNumero(%d)", valor);
+                    byte bits = this -> getBits(valor);
+                    
+                    for (SalidaDigital *selec: this -> salidasDatos) {
+                        selec -> setEstado(bits & 0b00000001);
+                        bits >>= 1;
+                    }
+                LOG("FIN Display7SegmentosParalelo::setNumero(%#x)", valor);
+            }
+            
+            void setEstado(bool valor) override {
+                this -> salidaEnable -> setEstado(valor);
+            }
         
-            size_t printTo(Print& impresora) const override;
+            size_t printTo(Print& impresora) const override {
+                return (imprimirCabeceraJSON(impresora, F("Display7SegmentosParalelo"))
+                    + imprimirVariableJSON(impresora, F("salidasDatos"), this -> salidasDatos) + impresora.print(JSON_SEPARADOR)
+                    + imprimirVariableJSON(impresora, F("salidaEnable"), this -> salidaEnable) + impresora.print(JSON_CLAUSURA_OBJETO)
+                );
+            }
     };
 #endif
