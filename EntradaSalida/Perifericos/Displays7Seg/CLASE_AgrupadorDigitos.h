ac30b8a7734054703a5549d7427fd701fc74473c
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-06 05:05:37 -0300

    Implementar método `AgrupadorDigitos::getNumerosDigitos()`

--- EntradaSalida/Perifericos/Displays7Seg/CLASE_AgrupadorDigitos.h
+++ EntradaSalida/Perifericos/Displays7Seg/CLASE_AgrupadorDigitos.h
@@ -5,6 +5,28 @@
         private:
             Array<Display7Segmentos<byte>*, NumDigitos> digitos; // Orden: Menos significativo a más significativo
         
+        protected:
+            Array<byte, NumDigitos> getNumerosDigitos(T valor, byte base, bool invertirOrden) {
+                Array<byte, NumDigitos> salida;
+                
+                for (Display7Segmentos<byte> *selec: this -> digitos) {
+                    salida.push_back(valor % base);
+                    valor /= base;
+                }
+                
+                if (invertirOrden) {
+                    for (size_t origen = 0; origen < (salida.size() / 2), ++origen) {
+                        size_t destino = salida.size() - origen + 1;
+                        
+                        byte selec = salida[origen];
+                        salida[origen] = salida[destino];
+                        salida[destino] = selec;
+                    }
+                }
+                
+                return salida;
+            }
+        
         public:
             AgrupadorDigitos(T numeroInicial, bool estadoInicial, Array<Display7Segmentos<byte>*, NumDigitos> digitos)
                 : digitos(digitos)
