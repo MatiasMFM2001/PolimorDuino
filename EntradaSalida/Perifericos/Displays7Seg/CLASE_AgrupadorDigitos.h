3f0929a095d49b42f593f3f04217c1485baaca00
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-06 04:42:03 -0300

    Mover VI `baseNumerica` de clase `AgrupadorDigitos` a parámetro opcional de método `AgrupadorDigitos::setNumero()`

--- EntradaSalida/Perifericos/Displays7Seg/CLASE_AgrupadorDigitos.h
+++ EntradaSalida/Perifericos/Displays7Seg/CLASE_AgrupadorDigitos.h
@@ -4,18 +4,15 @@
     class AgrupadorDigitos : Display7Segmentos<T> {
         private:
             Array<Display7Segmentos<byte>*, NumDigitos> digitos; // Orden: Menos significativo a más significativo
-            byte baseNumerica;
         
         public:
-            AgrupadorDigitos(T numeroInicial, bool estadoInicial, Array<Display7Segmentos<byte>*, NumDigitos> digitos, byte baseNumerica)
-                : digitos(digitos), baseNumerica(baseNumerica)
+            AgrupadorDigitos(T numeroInicial, bool estadoInicial, Array<Display7Segmentos<byte>*, NumDigitos> digitos)
+                : digitos(digitos)
             {
                 Display7Segmentos::Display7Segmentos(numeroInicial, estadoInicial);
             }
             
-            void setNumero(T valor) override {
-                byte base = this -> baseNumerica;
-                
+            void setNumero(T valor, byte base = DEC) override {
                 for (Display7Segmentos<byte> *selec: this -> digitos) {
                     selec -> setNumero(valor % base);
                     valor /= base;
