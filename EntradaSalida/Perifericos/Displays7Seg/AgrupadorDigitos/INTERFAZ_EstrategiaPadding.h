05e445b07a4ea14092aca3f470872d69dabb5dcc
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-11 03:11:55 -0300

    Hacer que el método `EstrategiaPadding::aplicarPadding()` sea abstracto

--- EntradaSalida/Perifericos/Displays7Seg/AgrupadorDigitos/INTERFAZ_EstrategiaPadding.h
+++ EntradaSalida/Perifericos/Displays7Seg/AgrupadorDigitos/INTERFAZ_EstrategiaPadding.h
@@ -5,6 +5,6 @@
     template <size_t NumDigitos>
     class EstrategiaPadding {
         public:
-            void aplicarPadding(Array<byte, NumDigitos> &digitos, Array<byte, NumDigitos> &salida);
+            virtual void aplicarPadding(Array<byte, NumDigitos> &digitos, Array<byte, NumDigitos> &salida) = 0;
     };
 #endif
