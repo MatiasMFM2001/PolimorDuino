00671a18d0f6d70e25872f5077625ccaa92821f7
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-11 15:56:49 -0300

    Mejorar implementación de `Sin0PaddingIzquierda::aplicarPadding()`

--- EntradaSalida/Perifericos/Displays7Seg/AgrupadorDigitos/CLASE_Sin0PaddingIzquierda.h
+++ EntradaSalida/Perifericos/Displays7Seg/AgrupadorDigitos/CLASE_Sin0PaddingIzquierda.h
@@ -2,17 +2,13 @@
 #define SIN_0PADDING_IZQUIERDA
 
 #include "INTERFAZ_EstrategiaPadding.h"
+#include "../../../../Utils/FuncionesGlobales.h"
     template <size_t NumDigitos>
     class Sin0PaddingIzquierda {
         public:
             void aplicarPadding(Array<byte, NumDigitos> &digitos, Array<byte, NumDigitos> &salida) override {
-                for (byte selec: digitos) {
-                    salida.push_back(selec);
-                }
-
-                for (size_t cont = digitos.size(), cont < salida.size(), ++cont) {
-                    salida.push_back(DISPLAY7SEG_APAGADO);
-                }
+                agregarFinal(digitos, salida);
+                completar(salida, DISPLAY7SEG_APAGADO);
             }
     };
 #endif
