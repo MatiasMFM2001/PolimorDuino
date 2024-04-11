5d7433ec300a67ae9e1b64d81264437f44ea19ad
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-11 16:03:58 -0300

    Crear e implementar clase `Sin0PaddingDerecha`

---
+++ EntradaSalida/Perifericos/Displays7Seg/AgrupadorDigitos/CLASE_Sin0PaddingDerecha.h
@@ -0,0 +1,14 @@
+#ifndef SIN_0PADDING_DERECHA
+#define SIN_0PADDING_DERECHA
+
+#include "INTERFAZ_EstrategiaPadding.h"
+#include "../../../../Utils/FuncionesGlobales.h"
+    template <size_t NumDigitos>
+    class Sin0PaddingDerecha {
+        public:
+            void aplicarPadding(Array<byte, NumDigitos> &digitos, Array<byte, NumDigitos> &salida) override {
+                completar(salida, DISPLAY7SEG_APAGADO, NumDigitos - digitos.size());
+                agregarFinal(digitos, salida);
+            }
+    };
+#endif
