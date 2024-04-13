93b2afc3dbb9f37554efd53805781106c3643eeb
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-13 03:44:42 -0300

    Crear sobrecarga de `imprimirDatoJSON()` y macros OBJETO_A_JSON_RECURSIVO()` y `OBJETO_A_JSON()`

--- Logger/FuncionesJSON.h
+++ Logger/FuncionesJSON.h
@@ -13,6 +13,7 @@
 #include <Arduino.h>
 #include <Print.h>
 #include "CLASE_WrapperPuntero.h"
+#include <avr/pgmspace.h>
     size_t imprimirCaracterJSON(Print& impresora, const char ingr);
     
     size_t imprimirDatoJSON(Print& impresora, const char* valor);
@@ -21,21 +22,26 @@
     size_t imprimirDatoJSON(Print& impresora, const Printable& valor);
     
     template <typename TValor>
+    size_t imprimirDatoJSON(Print& impresora, const TValor &valor) {
+        return impresora.print(valor);
+    }
+    
+    template <typename TValor>
     size_t imprimirDatoJSON(Print& impresora, const TValor* valor) {
         return imprimirDatoJSON(impresora, WrapperPuntero<TValor>(valor));
     }
     
-    //#define imprimirDatoJSON(impresora, valor, ...) imprimirDatoJSON(impresora, valor, ##__VA_ARGS__)
-    #define imprimirNombreVariableJSON(impresora, nombreVariable) (imprimirDatoJSON(impresora, nombreVariable) + impresora.print(JSON_VARIABLE))
-    //#define imprimirDatoJSON(impresora, valor, ...) impresora.print(valor, ##__VA_ARGS__)
-    #define imprimirVariableJSON(impresora, nombreVariable, valor, ...) (imprimirNombreVariableJSON(impresora, nombreVariable) + imprimirDatoJSON(impresora, valor, ##__VA_ARGS__))
-    
+    #define IMPRIMIR_NOMBRE_VARIABLE_JSON(impresora, nombreVariable) (imprimirDatoJSON(impresora, F(nombreVariable)) + impresora.print(JSON_VARIABLE))
+    #define IMPRIMIR_VARIABLE_JSON(impresora, nombreVariable, valor) (IMPRIMIR_NOMBRE_VARIABLE_JSON(impresora, nombreVariable) + imprimirDatoJSON(impresora, valor))
     
     template <typename TNombre>
     size_t imprimirCabeceraJSON(Print& impresora, const TNombre* nombreClase, bool objetoVacio = false) {
         return (impresora.print(JSON_APERTURA_OBJETO)
-            + imprimirVariableJSON(impresora, F("Clase"), nombreClase)
+            + IMPRIMIR_VARIABLE_JSON(impresora, "Clase", nombreClase)
             + impresora.print(objetoVacio ? JSON_CLAUSURA_OBJETO : JSON_SEPARADOR)
         );
     }
+    
+    #define OBJETO_A_JSON_RECURSIVO(impresora, selec, ...) (IMPRIMIR_VARIABLE_JSON(impresora, #selec, selec) + OBJETO_A_JSON_RECURSIVO(impresora, ##__VA_ARGS__))
+    #define OBJETO_A_JSON(impresora, nombreClase, ...) (imprimirCabeceraJSON(impresora, nombreClase) + OBJETO_A_JSON_RECURSIVO(impresora, ##__VA_ARGS__) + impresora.print(JSON_CLAUSURA_OBJETO))
 #endif
