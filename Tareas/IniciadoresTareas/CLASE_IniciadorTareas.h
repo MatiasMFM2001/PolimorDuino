e2b81c20ffe2dc1211e32c28bb6ebfc6a5e82eae
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-16 04:00:03 -0300

    Añadir documentación a archivo `Tareas/IniciadoresTareas/CLASE_IniciadorTareas.h`

--- Tareas/IniciadoresTareas/CLASE_IniciadorTareas.h
+++ Tareas/IniciadoresTareas/CLASE_IniciadorTareas.h
@@ -3,14 +3,41 @@
 
 #include "../../Inclusiones/InclusionTaskSchedulerDeclarations.h"
 #include <Printable.h>
+    /**
+     * @brief Tarea que permite iniciar la ejecución de otra tarea, cada cierta
+     *  cantidad de tiempo.
+     */
     class IniciadorTareas : public Task, public Printable {
         private:
+            /** @brief La tarea a iniciar periódicamente. */
             Task *tarea;
         
         public:
+            /**
+             * @brief Construye un IniciadorTareas, con el tiempo entre
+             *  iniciaciones, scheduler y tarea especificados.
+             *
+             * @param msEntreIniciaciones La cantidad de milisegundos entre
+             *  una iniciación de tarea y la siguiente.
+             * @param planif El planificador de tareas especificado (que puede
+             *  ser nulo).
+             * @param tarea La tarea especificada (que no debería ser nula).
+             */
             IniciadorTareas(unsigned long msEntreIniciaciones, Scheduler *planif, Task *tarea);
 
+            /**
+             * @brief Si la tarea ya estaba activada y @c deboFinalizar(),
+             *  se deshabilita.
+             *
+             * @returns @c true para indicar que la ejecución de la tarea fue
+             *  "productiva".
+             */
             bool Callback(void) override;
+
+            /**
+             * @returns @c true si hay que dejar de iniciar la tarea, @c false
+             *  en caso contrario.
+             */
             virtual bool deboFinalizar(void) = 0;
     };
 #endif
