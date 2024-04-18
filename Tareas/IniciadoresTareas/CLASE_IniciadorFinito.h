416dc95072e4051ac2952ce5ad95984623c654cc
Matías Manzin <matias.manzin@outlook.com.ar>
2024-04-17 21:08:44 -0300

    Añadir documentación a archivo `Tareas/IniciadoresTareas/CLASE_IniciadorFinito.h`

--- Tareas/IniciadoresTareas/CLASE_IniciadorFinito.h
+++ Tareas/IniciadoresTareas/CLASE_IniciadorFinito.h
@@ -3,14 +3,43 @@
 
 #include "CLASE_IniciadorTareas.h"
 #include "../../Utils/CLASE_Contador.h"
+    /**
+     * @brief Tarea que permite iniciar la ejecución de otra tarea, cada cierta
+     *  cantidad de tiempo, una cantidad finita de veces.
+     */
     class IniciadorFinito : public IniciadorTareas {
         private:
+            /**
+             * @brief El contador que almacena la cantidad de iniciaciones
+             *  restantes.
+             */
             Contador<size_t> contIteraciones;
         
         public:
+            /**
+             * @brief Construye un PinEntradaAnalogica, con el número de pin,
+             *  configuración de inversión y estado inicial especificados.
+             *
+             * @param numPin El número de pin especificado.
+             * @param invertir @c true para invertir el funcionamiento,
+             *  @c false para el caso contrario.
+             */
             IniciadorFinito(unsigned long msEntreIniciaciones, unsigned long numIniciaciones, Scheduler *planif, Task *tarea);
             
+            /**
+             * @brief Reinicia el contador de iteraciones.
+             *
+             * @returns @c true para indicar que la tarea debería permanecer
+             *  habilitada.
+             */
             bool OnEnable(void) override;
+
+            /**
+             * @brief Decrementa al contador de iteraciones en @c 1.
+             *
+             * @returns @c true si el valor de @c contIteraciones es @c 0,
+             *  @c false en caso contrario.
+             */
             bool deboFinalizar(void) override;
 
             /**
