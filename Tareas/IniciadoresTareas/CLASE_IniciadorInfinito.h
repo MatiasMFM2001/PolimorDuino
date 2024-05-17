8ec00005f895579e1711520ef6b1b65446804b36
Matías Manzin <matias.manzin@outlook.com.ar>
2024-05-17 00:14:17 -0300

    Añadir documentación a archivo `Tareas/IniciadoresTareas/CLASE_IniciadorInfinito.h`

--- Tareas/IniciadoresTareas/CLASE_IniciadorInfinito.h
+++ Tareas/IniciadoresTareas/CLASE_IniciadorInfinito.h
@@ -2,10 +2,27 @@
 #define INICIADOR_INFINITO
 
 #include "CLASE_IniciadorTareas.h"
+    /**
+     * @brief Tarea que permite iniciar la ejecución de otra tarea, cada cierta
+     *  cantidad de tiempo, una cantidad infinita de veces.
+     */
     class IniciadorInfinito : public IniciadorTareas {
         public:
+            /**
+             * @brief Construye un IniciadorInfinito, con el tiempo entre
+             *  iniciaciones, scheduler y tarea especificados.
+             *
+             * @param msEntreIniciaciones La cantidad de milisegundos entre
+             *  una iniciación de tarea y la siguiente.
+             * @param planif El planificador de tareas especificado (que puede
+             *  ser nulo).
+             * @param tarea La tarea especificada (que no debería ser nula).
+             */
             IniciadorInfinito(unsigned long msEntreIniciaciones, Scheduler *planif, Task *tarea);
             
+            /**
+             * @returns @c false para mantener la iniciación infinita.
+             */
             bool deboFinalizar(void) override;
 
             /**
