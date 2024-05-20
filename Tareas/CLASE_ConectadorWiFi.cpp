7e00851d508a2d3443ff7f83b4dced01620541fe
Matías Manzin <matias.manzin@outlook.com.ar>
2024-05-19 22:58:30 -0300

    Crear e implementar clase `ConectadorWiFi`

---
+++ Tareas/CLASE_ConectadorWiFi.cpp
@@ -0,0 +1,31 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "CLASE_ConectadorWiFi.h"
+#include <Arduino.h>
+#include "../Inclusiones/InclusionLog4Arduino.h"
+#include <WiFi.h>
+
+ConectadorWiFi::ConectadorWiFi(long msEntreLlamados, Scheduler *planif, CallbackResultado<void *> *notificadorConexionExitosa, char *nombreRed, char *contrasenia);
+    : Task(msEntreLlamados, TASK_FOREVER, planif, false)
+    , notificadorConexionExitosa(notificadorConexionExitosa), nombreRed(nombreRed), contrasenia(contrasenia)
+{}
+
+void ConectadorWiFi::inicializar(void) {
+    Task::enable();
+    WiFi.begin(this -> nombreRed, this -> contrasenia);
+}
+
+bool ConectadorWiFi::Callback(void) {
+    LOG("EJECUTANDO ConectadorWiFi::Callback()");
+    
+    if (WiFi.status() == WL_CONNECTED) {
+        this -> notificadorConexionExitosa -> notificar(WiFi);
+        Task::disable();
+    }
+
+    return true;
+}
---
+++ Tareas/CLASE_ConectadorWiFi.h
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef CONECTADOR_WIFI
+#define CONECTADOR_WIFI
+
+#include "../Inclusiones/InclusionTaskSchedulerDeclarations.h"
+#include "../Utils/INTERFAZ_Inicializable.h"
+    class ConectadorWiFi : public Task, public Inicializable {
+        private:
+            CallbackResultado<WiFiClass &> *notificadorConexionExitosa;
+            char *nombreRed;
+            char *contrasenia;
+        
+        public:
+            /**
+             * @brief Construye un AutoFlusher, con la impresora, tiempo de
+             *  espera y scheduler especificados.
+             * 
+             * @param impresora La impresora especificada (que no debería ser
+             *  nula).
+             * @param msEntreLlamados La cantidad mínima de milisegundos a
+             *  esperar entre una impresión y la siguiente.
+             * @param planif El planificador de tareas especificado (que puede
+             *  ser nulo).
+             */
+            ConectadorWiFi(long msEntreLlamados, Scheduler *planif, CallbackResultado<void *> *notificadorConexionExitosa, char *nombreRed, char *contrasenia);
+        
+            void inicializar(void) override;
+        
+            /**
+             * @brief Ejecuta la impresión periódica.
+             *
+             * @returns @c true para indicar que la ejecución de la tarea fue
+             *  "productiva".
+             */
+            bool Callback(void) override;
+    };
+#endif
