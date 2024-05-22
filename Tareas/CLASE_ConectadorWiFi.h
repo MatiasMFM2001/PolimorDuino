8f39cb4a97754fcb0e097ac87995751d5796cf13
Matías Manzin <matias.manzin@outlook.com.ar>
2024-05-22 00:20:09 -0300

    Hacer que `ConectadorWiFi` guarde el nombre y la contraseña en arrays de caracteres

--- Tareas/CLASE_ConectadorWiFi.h
+++ Tareas/CLASE_ConectadorWiFi.h
@@ -11,20 +11,32 @@
 #include "../Utils/INTERFAZ_Inicializable.h"
 #include <WiFi.h>
 #include "../Medidores/INTERFAZ_CallbackResultado.h"
+#include "../Utils/FuncionesGlobales.h"
+    template <size_t TAMANIO_NOMBRE = 32, size_t TAMANIO_CONTRASENIA = 32>
     class ConectadorWiFi : public Task, public Inicializable {
         private:
             CallbackResultado<WiFiClass &> *notificadorConexionExitosa;
-            char *nombreRed;
-            char *contrasenia;
+            Array<char, TAMANIO_NOMBRE + 1> nombreRed;
+            Array<char, TAMANIO_CONTRASENIA + 1> contrasenia;
         
         public:
             /**
              * @brief Construye un ConectadorWiFi...
              */
-            ConectadorWiFi(long msEntreLlamados, Scheduler *planif, CallbackResultado<WiFiClass &> *notificadorConexionExitosa);
+            ConectadorWiFi(long msEntreLlamados, Scheduler *planif, CallbackResultado<WiFiClass &> *notificadorConexionExitosa)
+                : Task(msEntreLlamados, TASK_FOREVER, planif, false)
+                , notificadorConexionExitosa(notificadorConexionExitosa), nombreRed(Array<char, TAMANIO_NOMBRE + 1>()), contrasenia(Array<char, TAMANIO_CONTRASENIA + 1>())
+            {}
         
-            void inicializar(void) override;
-            void setCredenciales(char *nombreRed, char *contrasenia);
+            void inicializar(void) override {
+                Task::enable();
+                WiFi.begin(this -> nombreRed.data(), this -> contrasenia.data());
+            }
+            
+            void setCredenciales(const char *nombreRed, const char *contrasenia) {
+                agregarFinalArrayTerminado(nombreRed, this -> nombreRed, '\0', true);
+                agregarFinalArrayTerminado(contrasenia, this -> contrasenia, '\0', true);
+            }
         
             /**
              * @brief Ejecuta la impresión periódica.
@@ -32,6 +44,15 @@
              * @returns @c true para indicar que la ejecución de la tarea fue
              *  "productiva".
              */
-            bool Callback(void) override;
+            bool Callback(void) override {
+                LOG("EJECUTANDO ConectadorWiFi::Callback()");
+                
+                if (WiFi.status() == WL_CONNECTED) {
+                    this -> notificadorConexionExitosa -> notificar(WiFi);
+                    Task::disable();
+                }
+
+                return true;
+            }
     };
 #endif
