d531ce71a8b2db3c6c4707c7b47219bf91695fbd
Matías Manzin <matias.manzin@outlook.com.ar>
2024-06-07 01:51:41 -0300

    Crear e implementar macros `IMPRIMIR_DATO()`, `CLOG_REFERENCIA_IMPRESORA()`, `CLOG_PUNTERO_IMPRESORA()`, `CLOG()`

--- Logger/FuncionesLoggers.h
+++ Logger/FuncionesLoggers.h
@@ -9,11 +9,19 @@
 
 #include <Printable.h>
 #include "../Inclusiones/InclusionLog4Arduino.h"
+#include "MACRO_ForEachAcumulando.h"
+    #define IMPRIMIR_DATO(impresora, valor) (impresora).print(valor); (impresora).print(' ');
+    #define CLOG_REFERENCIA_IMPRESORA(impresora, ...) FOR_EACH_ACUMULANDO(IMPRIMIR_DATO, (impresora), __VA_ARGS__); (impresora).print('\n');
+    #define CLOG_PUNTERO_IMPRESORA(impresora, ...) if (impresora) {CLOG_REFERENCIA_IMPRESORA(*impresora, __VA_ARGS__)}
+    
+    #ifdef ENABLE_LOG4ARDUINO
+        #define CLOG(...) log4arduino_print_head(); CLOG_PUNTERO_IMPRESORA(_log4arduino_target);
+    #else
+        #define CLOG(...)
+    #endif
+    
     template <typename T>
     void imprimir(T &valor) {
-        #ifdef ENABLE_LOG4ARDUINO
-            log4arduino_print_head();
-            _log4arduino_target -> println(valor);
-        #endif
+        CLOG(valor);
     }
 #endif
