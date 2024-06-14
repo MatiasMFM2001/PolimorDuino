4631b837484c0b8247675e800cc89f1055c2c23f
Matías Manzin <matias.manzin@outlook.com.ar>
2024-06-14 04:19:52 -0300

    Renombrar clase `ConectadorTelegramAsincronico` a `ConectadorTelegram` y hacer que reciba una instancia de `ClienteTelegram`

---
+++ Medidores/ComandosJSON/Telegram/CLASE_ConectadorTelegram.h
@@ -0,0 +1,55 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef CONECTADOR_TELEGRAM
+#define CONECTADOR_TELEGRAM
+
+#include "../../Instantaneos/CLASE_MedidorInstantaneo.h"
+#include "CLASE_ClienteTelegram.h"
+    /**
+     * @brief 
+     */
+    template <size_t CAPACIDAD_CANALES_PERMITIDOS, size_t CAPACIDAD_MENSAJE, void (*F_LOGGER_CLIENTE)(WrapperPuntero<Stream>&) = nullptr, void (*F_LOGGER)(WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>&) = nullptr>
+    class ConectadorTelegram : public MedidorInstantaneo<WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>, F_LOGGER>, public CondicionResultado<WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>>, public CondicionResultado<> {
+        private:
+            ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE> *bot;
+        
+        public:
+            ConectadorTelegram(const __FlashStringHelper *nombre, CallbackResultado<WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>> *callback, Scheduler *planif, ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE> *bot)
+                : MedidorInstantaneo<WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>, F_LOGGER>(nombre, callback, planif, this)
+                , bot(bot)
+            {}
+            
+            WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>> getResultado(void) override {
+                return WrapperPuntero(this -> bot);
+            }
+            
+            bool esValido(WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>> &resultado) override {
+                bool retorno = resultado.getDato().conectarseATelegram();
+                LOG("ESTADO DE INICIACIÓN = %s", retorno ? "true" : "false");
+                
+                return retorno;
+            }
+            
+            bool esValido(void) override {
+                bool retorno = this -> bot -> conectarseATelegram();
+                
+                LOG("ESTADO DE CONEXION = %s", retorno ? "true" : "false");
+                return retorno;
+            }
+            
+            /**
+             * @brief Imprime los valores de las variables de instancia a la
+             *  impresora especificada.
+             *
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
+            size_t printTo(Print &impresora) const override {
+                return OBJETO_A_JSON(impresora, "ConectadorTelegram") + SUPERCLASES_A_JSON(impresora, (Medidor<WrapperPuntero<ClienteTelegram<CAPACIDAD_CANALES_PERMITIDOS, CAPACIDAD_MENSAJE, F_LOGGER_CLIENTE>>, F_LOGGER>));
+            }
+    };
+#endif
