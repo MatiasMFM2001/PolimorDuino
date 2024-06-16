600253fb17985fe4b2c502f14593509396df1aac
Matías Manzin <matias.manzin@outlook.com.ar>
2024-06-16 00:42:16 -0300

    Agregar VI `nombre` y logger a `CallbackIniciador`

--- Medidores/Callbacks/CLASE_CallbackIniciador.h
+++ Medidores/Callbacks/CLASE_CallbackIniciador.h
@@ -8,25 +8,32 @@
 #define CALLBACK_INICIADOR
 
 #include <Printable.h>
+#include "../../Inclusiones/InclusionLog4Arduino.h"
     template <typename... T>
     class CallbackIniciador: public CallbackResultado<T...> {
         private:
             Task *tarea;
             CallbackResultado<T...> *accionesAdicionales;
+            const char *nombre;
         
         public:
-            CallbackIniciador(Task *tarea, CallbackResultado<T...> *accionesAdicionales = nullptr)
-                : tarea(tarea), accionesAdicionales(accionesAdicionales)
+            CallbackIniciador(const char *nombre, Task *tarea, CallbackResultado<T...> *accionesAdicionales = nullptr)
+                : tarea(tarea), accionesAdicionales(accionesAdicionales), nombre(nombre)
             {}
             
             void notificar(T&... resultado) override {
+                LOG("INICIO CallbackIniciador::notificar('%s')", this -> nombre);
+                
                 if (this -> tarea -> isEnabled()) {
+                    LOG("CallbackIniciador::notificar('%s') - Saliendo porque la tarea ya estaba habilitada", this -> nombre);
                     return;
                 }
                 
+                LOG("CallbackIniciador::notificar('%s') - Reiniciando tarea", this -> nombre);
                 this -> tarea -> restart();
                 
                 if (this -> accionesAdicionales) {
+                    LOG("CallbackIniciador::notificar('%s') - Ejecutando callback con acciones adicionales", this -> nombre);
                     this -> accionesAdicionales -> notificar(resultado...);
                 }
             }
