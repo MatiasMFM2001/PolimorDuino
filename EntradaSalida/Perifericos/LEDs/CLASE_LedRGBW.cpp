afbde0be96e7abf704242e2fc3ff50644bd1ff4b
Matías Manzin <matias.manzin@outlook.com.ar>
2024-06-19 01:20:19 -0300

    Usar parámetro anónimo en implementación de `LedRGBW::setEstado()`

--- EntradaSalida/Perifericos/LEDs/CLASE_LedRGBW.cpp
+++ EntradaSalida/Perifericos/LEDs/CLASE_LedRGBW.cpp
@@ -55,7 +55,7 @@ void LedRGBW::setColorEncendido(ColorRGBW ingr, bool mostrarSiEncendido) {
     this -> setColor(POS_ENCENDIDO, ingr, mostrarSiEncendido && (this -> getEstado()));
 }
 
-void LedRGBW::setEstado([[maybe_unused]] bool valor) {
+void LedRGBW::setEstado(bool) {
     this -> mostrar();
 }
 
