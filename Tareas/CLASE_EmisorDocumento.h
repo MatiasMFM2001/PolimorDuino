f00cf55991257ec894a34cb67b5a470df09b175d
Matías Manzin <matias.manzin@outlook.com.ar>
2024-06-20 16:52:13 -0300

    Crear e implementar clase `EmisorDocumento`

---
+++ Tareas/CLASE_EmisorDocumento.h
@@ -0,0 +1,28 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef EMISOR_DOCUMENTO
+#define EMISOR_DOCUMENTO
+
+#include "../Inclusiones/InclusionTaskSchedulerDeclarations.h"
+#include <Print.h>
+    template <size_t (*serializador)(const JsonDocument&, Print&)>
+    class EmisorDocumento : public Task {
+        private:
+            Print &salida;
+            JsonDocument *documento;
+
+        public:
+            EmisorDocumento(Print &salida, JsonDocument *documento = nullptr)
+                : salida(salida), documento(documento)
+            {}
+
+            bool OnEnable(void) override {
+                serializador(*(this -> documento), this -> salida);
+                return true;
+            }
+    };
+#endif
