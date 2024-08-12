4b95eb0596fa1fefdeb336001b0f312ec7f21620
Matías Manzin <matias.manzin@outlook.com.ar>
2024-08-12 20:54:08 -0300

    Crear e implementar clase `AdaptadorFuncionCallbackSinParametros`

---
+++ Medidores/Callbacks/CLASE_AdaptadorFuncionCallbackSinParametros.h
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef ADAPTADOR_FUNCION_CALLBACK_SIN_PARAMETROS
+#define ADAPTADOR_FUNCION_CALLBACK_SIN_PARAMETROS
+
+#include "INTERFAZ_CallbackResultado.h"
+#include "../../Logger/FuncionesJSON.h"
+    template <typename... T>
+    class AdaptadorFuncionCallbackSinParametros: public CallbackResultado<T...> {
+        private:
+            bool (*funcion)(void);
+        
+        public:
+            AdaptadorFuncionCallback(bool (*funcion)(void))
+                : funcion(funcion)
+            {}
+            
+            void notificar(T&... resultado) override {
+                this -> funcion();
+            }
+            
+            /**
+             * @brief Imprime los valores de las variables de instancia a la
+             *  impresora especificada.
+             *
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
+            size_t printTo(Print &impresora) const override {
+                return OBJETO_SIN_SUPER_A_JSON(impresora, "AdaptadorFuncionCallbackSinParametros");
+            }
+    };
+#endif
