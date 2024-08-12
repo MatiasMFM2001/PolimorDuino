a6cdc77b54c18bf367fd8cc8c698821b11a41f18
Matías Manzin <matias.manzin@outlook.com.ar>
2024-08-12 20:47:56 -0300

    Crer e implementar clase `AdaptadorFuncionCallback`

---
+++ Medidores/Callbacks/CLASE_AdaptadorFuncionCallback.h
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef ADAPTADOR_FUNCION_CALLBACK
+#define ADAPTADOR_FUNCION_CALLBACK
+
+#include "INTERFAZ_CallbackResultado.h"
+#include "../../Logger/FuncionesJSON.h"
+    template <typename... T>
+    class AdaptadorFuncionCallback: public CallbackResultado<T...> {
+        private:
+            bool (*funcion)(T...);
+        
+        public:
+            AdaptadorFuncionCallback(bool (*funcion)(T...))
+                : funcion(funcion)
+            {}
+            
+            void notificar(T&... resultado) override {
+                this -> funcion(resultado...);
+            }
+            
+            /**
+             * @brief Imprime los valores de las variables de instancia a la
+             *  impresora especificada.
+             *
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
+            size_t printTo(Print &impresora) const override {
+                return OBJETO_SIN_SUPER_A_JSON(impresora, "AdaptadorFuncionCallback");
+            }
+    };
+#endif
