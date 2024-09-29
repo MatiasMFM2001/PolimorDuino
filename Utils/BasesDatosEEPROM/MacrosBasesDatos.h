4c0dc121022604b9e9411e0e4729d5d52ec0e782
Matías Manzin <matias.manzin@outlook.com.ar>
2024-09-29 19:00:14 -0300

    Crear macros `CREAR_CALLBACKS_BD()`, `DEFINIR_CALLBACKS_BD()`, `DECLARAR_CALLBACKS_BD()`

---
+++ Utils/BasesDatosEEPROM/MacrosBasesDatos.h
@@ -0,0 +1,58 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "../../Medidores/ComandosJSON/CallbacksComandos/FuncionesComandos.h"
+
+#define CREAR_CALLBACKS_BD(nombreSettearBD, nombreVerBD, nombreGuardarBD) \
+    CREAR_COMANDO(CAPACIDAD_NOMBRES_INVC, nombreSettearBD, 3, 3), \
+    CREAR_COMANDO(CAPACIDAD_NOMBRES_INVC, nombreVerBD, 0, 0), \
+    CREAR_COMANDO(CAPACIDAD_NOMBRES_INVC, nombreGuardarBD, 0, 0)
+
+#define DEFINIR_CALLBACKS_BD(nombreSettearBD, nombreVerBD, nombreGuardarBD) \
+    DEFINIR_CALLBACK(nombreSettearBD); \
+    DEFINIR_CALLBACK(nombreVerBD); \
+    DEFINIR_CALLBACK(nombreGuardarBD);
+
+#define GUARDAR_DATO(baseDatos, punteroTipoDato, clave, tipoDato, valor, salida) \
+    if (strcmp(punteroTipoDato, #tipoDato) == 0) { \
+        VALIDAR_DATO_JSON(tipoDato, "El valor", valor, salida,); \
+        baseDatos.setValor(clave, valor.as<tipoDato>(), &Serial); \
+        salida.println("Valor guardado correctamente"); \
+    }
+
+#define DECLARAR_CALLBACKS_BD(baseDatos, nombreSettearBD, nombreVerBD, nombreGuardarBD) \
+    DECLARAR_CALLBACK(nombreSettearBD, args, numArgs, salida) { \
+        const char *clave = args[0]; \
+ \
+        if (clave[0] == '\0') { \
+            CLOG_REFERENCIA_IMPRESORA(salida, "ERROR: La clave ingresada es vacía"); \
+            return; \
+        } \
+ \
+        const char *tipoDato = args[1]; \
+        const JsonVariant &valor = args[2]; \
+ \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, int, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, size_t, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, float, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, const char *, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, bool, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, JsonArray, valor, salida); \
+        GUARDAR_DATO(baseDatos, tipoDato, clave, JsonObject, valor, salida); \
+    } \
+ \
+    DECLARAR_CALLBACK(nombreVerBD, args, numArgs, salida) { \
+        CLOG_REFERENCIA_IMPRESORA(salida, baseDatos); \
+    } \
+ \
+    DECLARAR_CALLBACK(nombreGuardarBD, args, numArgs, salida) { \
+        if (baseDatos.guardar(&salida)) { \
+            CLOG_REFERENCIA_IMPRESORA(salida, "BD guardada correctamente"); \
+        } \
+        else { \
+            CLOG_REFERENCIA_IMPRESORA(salida, "ERROR: No se pudo guardar el contenido de la BD"); \
+        } \
+    }
