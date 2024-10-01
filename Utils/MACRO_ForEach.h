25705666559f3c5b9e978e636241054119cbbea0
Matías Manzin <matias.manzin@outlook.com.ar>
2024-10-01 02:06:22 -0300

    Agregar más macros `FOR_EACH_N()`

--- Utils/MACRO_ForEach.h
+++ Utils/MACRO_ForEach.h
@@ -59,10 +59,62 @@
   what(x);\
   FOR_EACH_7(what,  __VA_ARGS__);
 
+#define FOR_EACH_9(what, x, ...)\
+  what(x);\
+  FOR_EACH_8(what,  __VA_ARGS__);
+
+#define FOR_EACH_10(what, x, ...)\
+  what(x);\
+  FOR_EACH_9(what,  __VA_ARGS__);
+
+#define FOR_EACH_11(what, x, ...)\
+  what(x);\
+  FOR_EACH_10(what,  __VA_ARGS__);
+
+#define FOR_EACH_12(what, x, ...)\
+  what(x);\
+  FOR_EACH_11(what,  __VA_ARGS__);
+
+#define FOR_EACH_13(what, x, ...)\
+  what(x);\
+  FOR_EACH_12(what, __VA_ARGS__);
+
+#define FOR_EACH_14(what, x, ...)\
+  what(x);\
+  FOR_EACH_13(what,  __VA_ARGS__);
+
+#define FOR_EACH_15(what, x, ...)\
+  what(x);\
+ FOR_EACH_14(what,  __VA_ARGS__);
+
+#define FOR_EACH_16(what, x, ...)\
+  what(x);\
+  FOR_EACH_15(what,  __VA_ARGS__);
+
+#define FOR_EACH_17(what, x, ...)\
+  what(x);\
+  FOR_EACH_16(what,  __VA_ARGS__);
+
+#define FOR_EACH_18(what, x, ...)\
+  what(x);\
+  FOR_EACH_17(what,  __VA_ARGS__);
+
+#define FOR_EACH_19(what, x, ...)\
+  what(x);\
+  FOR_EACH_18(what,  __VA_ARGS__);
+
+#define FOR_EACH_20(what, x, ...)\
+  what(x);\
+  FOR_EACH_19(what,  __VA_ARGS__);
+
+#define FOR_EACH_21(what, x, ...)\
+  what(x);\
+  FOR_EACH_20(what,  __VA_ARGS__);
+
 #define FOR_EACH_NARG(...) FOR_EACH_NARG_(__VA_ARGS__, FOR_EACH_RSEQ_N())
 #define FOR_EACH_NARG_(...) FOR_EACH_ARG_N(__VA_ARGS__) 
-#define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N // Retorna la cantidad de parámetros enviados a FOR_EACH()
-#define FOR_EACH_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, N, ...) N // Retorna la cantidad de parámetros enviados a FOR_EACH()
+#define FOR_EACH_RSEQ_N() 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 #define FOR_EACH_(N, what, ...) CONCATENATE(FOR_EACH_, N)(what, __VA_ARGS__)
 
