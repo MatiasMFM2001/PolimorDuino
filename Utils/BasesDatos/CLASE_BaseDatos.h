19c0c04938b5c62e4935bd0d78b9aa95232d8cc4
Matías Manzin <matias.manzin@outlook.com.ar>
2024-10-02 22:36:11 -0300

    Refactorizar clase `BaseDatosEEPROM`, creando superclase abstracta `BaseDatos`

---
+++ Utils/BasesDatos/CLASE_BaseDatos.h
@@ -0,0 +1,192 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef BASE_DATOS
+#define BASE_DATOS
+
+#define CLAVE_VERSION "VERSION"
+
+#define TIPOS_NATIVOS_BD bool, float, double, signed char, unsigned char, signed int, unsigned int, signed short, unsigned short, signed long, unsigned long, signed long long, unsigned long long
+#define TIPOS_CONVERTIBLES_BD JsonVariant, JsonArray, JsonObject
+
+#define GET_VALOR(clave, T, salida) \
+    if (this -> estaCorrupta) { \
+        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatos::getValor('", clave, "') con la BD corrupta. Se retornará el valor indeterminado"); \
+ \
+        if (this -> contieneClave(clave)) { \
+            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se retornará el valor previamente almacenado en esa clave."); \
+        } \
+        else { \
+            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se retornará el valor predeterminado para ese tipo de datos."); \
+        } \
+    } \
+ \
+    if (!(this -> contieneClave(clave))) { \
+        return false; \
+    }
+
+#define GET_VALOR_SETTEANDO(clave, T, valorPredeterminado, salida) \
+    if (this -> estaCorrupta) { \
+        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatos::getValorSetteando('", clave, "') con la BD corrupta. Se retornará el valor predeterminado"); \
+        return false; \
+    } \
+ \
+    if (!(this -> contieneClave(clave))) { \
+        this -> setValor(clave, valorPredeterminado); \
+    } \
+ \
+    if (this -> estaCorrupta) { \
+        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se corrompió la BD tras ejecutar BaseDatos::getValorSetteando('", clave, "')"); \
+ \
+        if (!(this -> contieneClave(clave))) { \
+            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se retornará el valor predeterminado"); \
+            return valorPredeterminado; \
+        } \
+    }
+
+#define SET_VALOR(clave, copiaValor, CAPACIDAD_STRINGS, salida) \
+    if (this -> estaCorrupta) { \
+        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatos::setValor('", clave, "') con la BD corrupta"); \
+        return false; \
+    } \
+ \
+    StringEstatica<CAPACIDAD_STRINGS> copiaClave(clave); \
+ \
+    CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatos::setValor('%s') = %s", clave, copiaClave.getContenido()); \
+ \
+    if (!(this -> escribirBajoNivel(copiaClave.getContenido(), copiaValor, salida))) { \
+        CLOG_PUNTERO_IMPRESORA(salida, "ERROR: Al intentar insertar la clave '", clave, "', falló porque la BaseDatos se llenó"); \
+        this -> estaCorrupta = true; \
+        return false; \
+    } \
+ \
+    CLOG_PUNTERO_IMPRESORA(salida, "BaseDatos::setValor('", clave, "') finalizó correctamente"); \
+    return true; \
+
+#define DECLARAR_METODO_ABSTRACTO_BD(nombre, retorno, atributosAdicionales, ...) \
+    virtual retorno nombre(__VA_ARGS__, Print *salida) atributosAdicionales = 0;
+
+#define DECLARAR_LEER_BAJO_NIVEL_ABSTRACTO_BD(T_VALOR) \
+    DECLARAR_METODO_ABSTRACTO_BD(leerBajoNivel, bool,, const char *clave, T_VALOR &variableASobreescribir);
+
+#define DECLARAR_ESCRIBIR_BAJO_NIVEL_ABSTRACTO_BD(T_VALOR) \
+    DECLARAR_METODO_ABSTRACTO_BD(escribirBajoNivel, bool,, char *copiaClave, T_VALOR copiaValor);
+
+#define DEFINIR_METODO_BD(nombre, retorno, atributosAdicionales, ...) \
+    retorno nombre(__VA_ARGS__, Print *salida) atributosAdicionales override
+
+#include <ArduinoJson.h>
+#include "../FuncionesGlobales.h"
+#include "../../Medidores/Callbacks/INTERFAZ_CallbackResultadoMutable.h"
+#include "../CLASE_StringEstatica.h"
+#include "../../Logger/FuncionesLoggers.h"
+#include "../MACRO_ForEach.h"
+#include "../INTERFAZ_Inicializable.h"
+    template <size_t CAPACIDAD_STRINGS>
+    class BaseDatos : public Inicializable, public Printable {
+        private:
+            bool leerAlInicializar;
+            bool estaCorrupta;
+            size_t version;
+            CallbackResultadoMutable<BaseDatos<CAPACIDAD_STRINGS>> *migrador;
+        
+        protected:
+            virtual bool inicializarBajoNivel(void) = 0;
+            
+            FOR_EACH(DECLARAR_LEER_BAJO_NIVEL_ABSTRACTO_BD, TIPOS_NATIVOS_BD);
+            FOR_EACH(DECLARAR_LEER_BAJO_NIVEL_ABSTRACTO_BD, TIPOS_CONVERTIBLES_BD);
+            
+            FOR_EACH(DECLARAR_ESCRIBIR_BAJO_NIVEL_ABSTRACTO_BD, TIPOS_NATIVOS_BD);
+            FOR_EACH(DECLARAR_ESCRIBIR_BAJO_NIVEL_ABSTRACTO_BD, TIPOS_CONVERTIBLES_BD);
+            
+            virtual bool contieneClaveBajoNivel(const char *ingr) = 0;
+            virtual bool guardarBajoNivel(Print *salida) = 0;
+        
+        public:
+            BaseDatos(size_t version, CallbackResultadoMutable<BaseDatos<CAPACIDAD_STRINGS>> *migrador, bool leerAlInicializar)
+                : leerAlInicializar(leerAlInicializar), estaCorrupta(false), version(version), migrador(migrador)
+            {}
+            
+            void inicializar(void) override {
+                if (!(this -> leerAlInicializar)) {
+                    return;
+                }
+                
+                if (!(this -> inicializarBajoNivel())) {
+                    FLOGS("BaseDatos::inicializar() - No se pudo inicializar correctamente");
+                    return;
+                }
+                
+                size_t versionDocumento;
+                this -> getValorSetteando(CLAVE_VERSION, 1U, versionDocumento);
+                
+                LOG("BaseDatos::inicializar() - MessagePack deserializado correctamente, de versión = %d", versionDocumento);
+                //imprimir(this -> documento);
+                
+                if ((versionDocumento >= (this -> version)) || !(this -> migrador)) {
+                    return;
+                }
+                
+                this -> migrador -> notificar(*this);
+                this -> setValor(CLAVE_VERSION, this -> version);
+                this -> guardar();
+            }
+            
+            template <typename T_VALOR>
+            bool getValor(const char *clave, T_VALOR &variableASobreescribir, Print *salida = nullptr) {
+                GET_VALOR(clave, T_VALOR, salida);
+                return (this -> leerBajoNivel(clave, variableASobreescribir, salida));
+            }
+            
+            template <typename T_VALOR>
+            bool setValor(const char *clave, const T_VALOR valor, Print *salida = nullptr) {
+                T_VALOR copiaValor = valor;
+                CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatos::setValor('", clave, "', ", valor, ')');
+                SET_VALOR(clave, copiaValor, CAPACIDAD_STRINGS, salida);
+            }
+            
+            bool setValor(const char *clave, const char *valor, Print *salida = nullptr) {
+                StringEstatica<CAPACIDAD_STRINGS> copiaValor(valor);
+                CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatos::setValor('", clave, "', '", valor, "')");
+                SET_VALOR(clave, copiaValor.getContenido(), CAPACIDAD_STRINGS, salida);
+            }
+            
+            template <typename T_VALOR>
+            bool getValorSetteando(const char *clave, T_VALOR valorPredeterminado, T_VALOR &variableASobreescribir, Print *salida = nullptr) {
+                GET_VALOR_SETTEANDO(clave, T_VALOR, valorPredeterminado, salida);
+                return (this -> leerBajoNivel(clave, variableASobreescribir, salida));
+            }
+            
+            bool guardar(Print *salida = nullptr) {
+                if (this -> estaCorrupta) {
+                    CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se accedió a BaseDatos::guardar() con la BD corrupta");
+                    return false;
+                }
+                
+                if (!(this -> guardarBajoNivel(salida))) {
+                    FLOGS("BaseDatos::guardar() - No se pudo guardar correctamente");
+                    return false;
+                }
+                
+                CLOG_PUNTERO_IMPRESORA(salida, "BaseDatos::guardar() - Entradas guardadas correctamente");
+                return true;
+            }
+            
+            bool contieneClave(const char *ingr) {
+                return (this -> contieneClaveBajoNivel(ingr));
+            }
+            
+            /**
+             * @brief Imprime el dato apuntado a la impresora especificada.
+             *
+             * @param impresora Referencia a la impresora especificada.
+             * @returns La cantidad de bytes escritos a la impresora.
+             */
+            size_t printTo(Print &impresora) const override {
+                return OBJETO_SIN_SUPER_A_JSON(impresora, "BaseDatos", leerAlInicializar, estaCorrupta, version, migrador);
+            }
+    };
+#endif
--- Utils/BasesDatos/CLASE_BaseDatosEEPROM.h
+++ Utils/BasesDatos/CLASE_BaseDatosEEPROM.h
@@ -10,21 +10,7 @@
 #define DIRECCION_NUM_BYTES (0)
 #define DIRECCION_DOCUMENTO (DIRECCION_NUM_BYTES + sizeof(size_t))
 
-#define CLAVE_VERSION "VERSION"
-
-#define GET_VALOR(clave, salida) \
-    if (this -> estaCorrupta) { \
-        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatosEEPROM::getValor('", clave, "') con la BD corrupta. Se retornará el valor indeterminado"); \
- \
-        if (this -> documento.containsKey(clave)) { \
-            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se retornará el valor previamente almacenado en esa clave."); \
-        } \
-        else { \
-            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se retornará el valor predeterminado para ese tipo de datos."); \
-        } \
-    }
-
-#define GET_VALOR_SETTEANDO(clave, valorPredeterminado, salida) \
+//#define GET_VALOR_SETTEANDO(clave, valorPredeterminado, salida) \
     if (this -> estaCorrupta) { \
         CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatosEEPROM::getValorSetteando('", clave, "') con la BD corrupta. Se retornará el valor predeterminado"); \
         return valorPredeterminado; \
@@ -43,78 +29,56 @@
         } \
     }
 
-#define SET_VALOR(clave, copiaValor, CAPACIDAD_STRINGS, salida) \
-    if (this -> estaCorrupta) { \
-        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se ejecutó BaseDatosEEPROM::setValor('", clave, "') con la BD corrupta"); \
-        return false; \
-    } \
- \
-    StringEstatica<CAPACIDAD_STRINGS> copiaClave(clave); \
- \
-    CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatosEEPROM::setValor('%s') = %s", clave, copiaClave.getContenido()); \
- \
-    this -> documento.garbageCollect(); \
-    this -> documento[copiaClave.getContenido()] = copiaValor; \
-    this -> documento.garbageCollect(); \
- \
-    if (this -> documento.overflowed()) { \
-        CLOG_PUNTERO_IMPRESORA(salida, "ERROR: Al intentar insertar la clave '", clave, "', falló porque el StaticJsonDocument<", CAPACIDAD_JSON, "> se llenó"); \
-        this -> estaCorrupta = true; \
-        return false; \
-    } \
- \
-    size_t tamanioSerializado = this -> medirSerializado(); \
-    size_t tamanioEEPROM = (this -> eeprom -> length() - DIRECCION_DOCUMENTO + 1); \
- \
-    if (tamanioSerializado > tamanioEEPROM) { \
-        CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Tras insertar la clave '", clave, "', el documento serializado queda de tamaño ", tamanioSerializado, " y no entra en la EEPROM (de capacidad ", tamanioEEPROM, ')'); \
-        this -> estaCorrupta = true; \
-        return false; \
-    } \
- \
-    CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::setValor('", clave, "') finalizó correctamente"); \
-    return true; \
+#define DEFINIR_LEER_BAJO_NIVEL_BD_EEPROM(T_VALOR) \
+    DEFINIR_METODO_BD(leerBajoNivel, bool,, const char *clave, T_VALOR &variableASobreescribir) { \
+        variableASobreescribir = (this -> documento[clave]); \
+        return true; \
+    }
 
-#include <ArduinoJson.h>
-#include "../FuncionesGlobales.h"
+#define DEFINIR_ESCRIBIR_BAJO_NIVEL_BD_EEPROM(T_VALOR) \
+    DEFINIR_METODO_BD(escribirBajoNivel, bool,, char *copiaClave, T_VALOR copiaValor) { \
+        this -> documento.garbageCollect(); \
+        this -> documento[copiaClave] = copiaValor; \
+        this -> documento.garbageCollect(); \
+ \
+        if (this -> documento.overflowed()) { \
+            CLOG_PUNTERO_IMPRESORA(salida, "ERROR: Al intentar insertar la clave '", copiaClave, "', falló porque el StaticJsonDocument<", CAPACIDAD_JSON, "> se llenó"); \
+            return false; \
+        } \
+ \
+        size_t tamanioSerializado = this -> medirSerializado(); \
+        size_t tamanioEEPROM = (this -> eeprom -> length() - DIRECCION_DOCUMENTO + 1); \
+ \
+        if (tamanioSerializado > tamanioEEPROM) { \
+            CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Tras insertar la clave '", copiaClave, "', el documento serializado queda de tamaño ", tamanioSerializado, " y no entra en la EEPROM (de capacidad ", tamanioEEPROM, ')'); \
+            return false; \
+        } \
+ \
+        CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::escribirBajoNivel('", copiaClave, "') finalizó correctamente"); \
+        return true; \
+    }
+
 #include "CLASE_LectorEEPROM.h"
 #include "CLASE_EscritorEEPROM.h"
 #include <EEPROM.h>
-#include "../../Medidores/Callbacks/INTERFAZ_CallbackResultado.h"
-#include <Printable.h>
-#include "../CLASE_StringEstatica.h"
-#include "../../Logger/FuncionesLoggers.h"
 #include <StreamUtils.h>
+#include "CLASE_BaseDatos.h"
     template <size_t CAPACIDAD_JSON, size_t CAPACIDAD_STRINGS, typename T_EEPROM = EEPROMClass>
-    class BaseDatosEEPROM : public Inicializable, public Printable {
+    class BaseDatosEEPROM : public BaseDatos<CAPACIDAD_STRINGS> {
         private:
             T_EEPROM *eeprom;
             StaticJsonDocument<CAPACIDAD_JSON> documento;
-            bool leerAlInicializar;
-            bool estaCorrupta;
-            size_t version;
-            CallbackResultado<JsonDocument> *migrador;
-            
-        public:
-            BaseDatosEEPROM(T_EEPROM *eeprom, size_t version, CallbackResultado<JsonDocument> *migrador, bool leerAlInicializar = false)
-                : eeprom(eeprom), documento(StaticJsonDocument<CAPACIDAD_JSON>()), leerAlInicializar(leerAlInicializar), estaCorrupta(false), version(version), migrador(migrador)
-            {}
-            
-            void inicializar(void) override {
-                if (!(this -> leerAlInicializar)) {
-                    return;
-                }
-                
+        
+        protected:
+             bool inicializarBajoNivel(void) override {
                 size_t numBytesUsados;
                 this -> eeprom -> get(DIRECCION_NUM_BYTES, numBytesUsados);
                 
-                LOG("BaseDatosEEPROM::inicializar() - numBytesUsados = %d", numBytesUsados);
+                LOG("BaseDatosEEPROM::inicializarBajoNivel() - numBytesUsados = %d", numBytesUsados);
                 
                 if (numBytesUsados > (this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1)) {
                     FLOGS("ERROR: La cantidad de bytes leidos es mayor a la posible de almacenar en la EEPROM");
-                    this -> estaCorrupta = true;
-                    
-                    return;
+                    return false;
                 }
                 
                 //LectorEEPROM lector(DIRECCION_DOCUMENTO, this -> eeprom, numBytesUsados);
@@ -122,10 +86,8 @@
                 DeserializationError retorno = deserializeMsgPack(this -> documento, lector);
                 
                 if (retorno != DeserializationError::Ok) {
-                    LOG("ERROR: Deserializar el MessagePack de la EEPROM falló con el error %d", retorno);
-                    this -> estaCorrupta = true;
-                    
-                    return;
+                    LOG("ERROR: Deserializar el MessagePack de la EEPROM falló con el error %s", retorno.c_str());
+                    return false;
                 }
                 
                 if (!(this -> documento.template is<JsonObject>())) {
@@ -133,68 +95,18 @@
                     this -> documento.clear();
                 }
                 
-                size_t versionDocumento = this -> getValorSetteando(CLAVE_VERSION, 1);
-                LOG("BaseDatosEEPROM::inicializar() - MessagePack deserializado correctamente, de versión = %d", versionDocumento);
-                imprimir(this -> documento);
-                
-                if (versionDocumento >= (this -> version) || !(this -> migrador)) {
-                    return;
-                }
-                
-                this -> migrador -> notificar(this -> documento);
-                this -> documento[CLAVE_VERSION] = (this -> version);
-                this -> guardar();
+                return true;
             }
             
-            size_t medirSerializado(void) {
-                return measureMsgPack(this -> documento);
-            }
+            FOR_EACH(DEFINIR_LEER_BAJO_NIVEL_BD_EEPROM, TIPOS_NATIVOS_BD);
+            FOR_EACH(DEFINIR_LEER_BAJO_NIVEL_BD_EEPROM, TIPOS_CONVERTIBLES_BD);
             
-            template <typename T>
-            T getValor(const char *clave, Print *salida = nullptr) {
-                GET_VALOR(clave, salida);
-                return this -> documento[clave];
-            }
+            FOR_EACH(DEFINIR_ESCRIBIR_BAJO_NIVEL_BD_EEPROM, TIPOS_NATIVOS_BD);
+            FOR_EACH(DEFINIR_ESCRIBIR_BAJO_NIVEL_BD_EEPROM, TIPOS_CONVERTIBLES_BD);
             
-            template <typename T>
-            const T &getValor(const char *clave, Print *salida = nullptr) const {
-                GET_VALOR(clave, salida);
-                return this -> documento[clave];
-            }
-            
-            template <typename T>
-            bool setValor(const char *clave, const T valor, Print *salida = nullptr) {
-                T copiaValor = valor;
-                CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatosEEPROM::setValor('", clave, "', ", valor, ')');
-                SET_VALOR(clave, copiaValor, CAPACIDAD_STRINGS, salida);
-            }
-            
-            bool setValor(const char *clave, const char *valor, Print *salida = nullptr) {
-                StringEstatica<CAPACIDAD_STRINGS> copiaValor(valor);
-                CLOG_PUNTERO_IMPRESORA(salida, "Escribiendo BaseDatosEEPROM::setValor('", clave, "', '", valor, "')");
-                SET_VALOR(clave, copiaValor.getContenido(), CAPACIDAD_STRINGS, salida);
-            }
-            
-            template <typename T>
-            const T getValorSetteando(const char *clave, const T valorPredeterminado, Print *salida = nullptr) const {
-                GET_VALOR_SETTEANDO(clave, valorPredeterminado, salida);
-                return this -> documento[clave];
-            }
-            
-            template <typename T>
-            T getValorSetteando(const char *clave, T valorPredeterminado, Print *salida = nullptr) {
-                GET_VALOR_SETTEANDO(clave, valorPredeterminado, salida);
-                return this -> documento[clave];
-            }
-            
-            bool guardar(Print *salida = nullptr) {
-                if (this -> estaCorrupta) {
-                    CLOG_PUNTERO_IMPRESORA(salida, "ADVERTENCIA: Se accedió a BaseDatosEEPROM::guardar() con la BD corrupta");
-                    return false;
-                }
-                
+            bool guardarBajoNivel(Print *salida) override {
                 //EscritorEEPROM escritor(DIRECCION_DOCUMENTO, this -> eeprom);
-                CLOG_PUNTERO_IMPRESORA(salida, "DIRECCION_DOCUMENTO =", DIRECCION_DOCUMENTO, "this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1 =", this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1);
+                CLOG_PUNTERO_IMPRESORA(salida, "DIRECCION_DOCUMENTO = ", DIRECCION_DOCUMENTO, "; this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1 = ", this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1);
                 EepromStream escritor(DIRECCION_DOCUMENTO, this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1);
                 size_t tamanioEscrito = serializeMsgPack(this -> documento, escritor);
                 
@@ -205,12 +117,10 @@
                 size_t numBytesUsadosReleido;
                 this -> eeprom -> get(DIRECCION_NUM_BYTES, numBytesUsadosReleido);
                 
-                CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::guardar() - numBytesUsadosReleido =", numBytesUsadosReleido);
+                CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::guardarBajoNivel() - numBytesUsadosReleido = ", numBytesUsadosReleido);
                 
                 if (numBytesUsadosReleido > (this -> eeprom -> length() - DIRECCION_DOCUMENTO - 1)) {
-                    LOG("ERROR: La cantidad de bytes releidos es mayor a la posible de almacenar en la EEPROM");
-                    this -> estaCorrupta = true;
-                    
+                    CLOG_PUNTERO_IMPRESORA(salida, "ERROR: La cantidad de bytes releidos es mayor a la posible de almacenar en la EEPROM");
                     return false;
                 }
 
@@ -218,25 +128,30 @@
                 
                 if (retorno != DeserializationError::Ok) {
                     CLOG_PUNTERO_IMPRESORA(salida, "ERROR: Re-deserializar el MessagePack de la EEPROM falló con el error", retorno.c_str());
-                    this -> estaCorrupta = true;
-                    
                     return false;
                 }
                 
                 if (!(this -> documento.template is<JsonObject>())) {
                     CLOG_PUNTERO_IMPRESORA(salida, "ERROR: El documento releido no es un objeto JSON");
-                    this -> estaCorrupta = true;
-                    
                     return false;
                 }
                 
-                CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::guardar() - Guardados", tamanioEscrito, '/', this -> eeprom -> length(), "bytes correctamente");
+                CLOG_PUNTERO_IMPRESORA(salida, "BaseDatosEEPROM::guardarBajoNivel() - Guardados", tamanioEscrito, '/', this -> eeprom -> length(), "bytes correctamente");
                 return true;
             }
             
-            template <typename T_VALOR>
-            bool contieneClave(const char *ingr) {
-                return (this -> documento.containsKey(ingr)) && (this -> documento[ingr].template is<T_VALOR>());
+            bool contieneClaveBajoNivel(const char *ingr) override {
+                return (this -> documento.containsKey(ingr));
+            }
+        
+        public:
+            BaseDatosEEPROM(T_EEPROM *eeprom, size_t version, CallbackResultadoMutable<BaseDatos<CAPACIDAD_STRINGS>> *migrador, bool leerAlInicializar = false)
+                : BaseDatos<CAPACIDAD_STRINGS>(version, migrador, leerAlInicializar)
+                , eeprom(eeprom), documento(StaticJsonDocument<CAPACIDAD_JSON>())
+            {}
+            
+            size_t medirSerializado(void) {
+                return measureMsgPack(this -> documento);
             }
             
             /**
@@ -246,7 +161,7 @@
              * @returns La cantidad de bytes escritos a la impresora.
              */
             size_t printTo(Print &impresora) const override {
-                return OBJETO_SIN_SUPER_A_JSON(impresora, "BaseDatosEEPROM", leerAlInicializar, estaCorrupta, version, migrador)
+                return OBJETO_A_JSON(impresora, "BaseDatosEEPROM") + SUPERCLASES_A_JSON(impresora, BaseDatos<CAPACIDAD_STRINGS>)
                     + impresora.println()
                     + serializeJsonPretty(this -> documento, impresora);
             }
