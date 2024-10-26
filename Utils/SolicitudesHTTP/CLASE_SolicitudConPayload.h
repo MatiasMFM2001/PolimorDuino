83a61319830d77c75617348cf39a861ee64a1a03
Matías Manzin <matias.manzin@outlook.com.ar>
2024-10-26 03:18:58 -0300

    Crear e implementar clases `SolicitudSinPayload` y `SolicitudConPayload`

---
+++ Utils/SolicitudesHTTP/CLASE_SolicitudConPayload.h
@@ -0,0 +1,37 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOLICITUD_CON_PAYLOAD
+#define SOLICITUD_CON_PAYLOAD
+
+#include "CLASE_SolicitudSinPayload.h"
+#include "../EstructurasDatos/Buffers/CLASE_BufferImpresora.h"
+    template <size_t CAPACIDAD_MAPA_ARGUMENTOS, size_t CAPACIDAD_MAPA_ENCABEZADOS, size_t CAPACIDAD_URI_COMPLETA, size_t CAPACIDAD_BUFFER_PAYLOAD>
+    class SolicitudConPayload : public SolicitudSinPayload<CAPACIDAD_MAPA_ARGUMENTOS, CAPACIDAD_MAPA_ENCABEZADOS, CAPACIDAD_URI_COMPLETA>, public Print {
+        private:
+            BufferImpresora<CAPACIDAD_BUFFER_PAYLOAD, uint8_t> buffer;
+        
+        protected:
+            virtual int enviarSolicitudBajoNivel(HTTPClient &cliente, const char *metodoCopiado) override {
+                return cliente.sendRequest(metodoCopiado, this -> buffer.getContenido(), this -> buffer.getLongitudBytes());
+            }
+        
+        public:
+            SolicitudConPayload(NetworkClient &clienteRed, CallbackResultado<HTTPClient, int> *callbackRecepcionRespuesta)
+                : SolicitudSinPayload<CAPACIDAD_MAPA_ARGUMENTOS, CAPACIDAD_MAPA_ENCABEZADOS, CAPACIDAD_URI_COMPLETA>(clienteRed, callbackRecepcionRespuesta)
+                , buffer(BufferImpresora<CAPACIDAD_BUFFER_PAYLOAD>())
+            {}
+            
+            size_t write(byte ingr) override {
+                return (this -> buffer.write(ingr));
+            }
+            
+            void flush(void) override {
+                this -> enviar();
+                this -> buffer.vaciarContenido();
+            }
+    };
+#endif
---
+++ Utils/SolicitudesHTTP/CLASE_SolicitudSinPayload.h
@@ -0,0 +1,120 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef SOLICITUD_SIN_PAYLOAD
+#define SOLICITUD_SIN_PAYLOAD
+
+#ifdef ESP8266
+    #include <ESP8266HTTPClient.h>
+#else
+    #include <HTTPClient.h>
+#endif
+
+#include "../EstructurasDatos/Mapas/CLASE_ArrayMapa.h"
+#include "../../Medidores/Callbacks/INTERFAZ_CallbackResultado.h"
+#include <UrlEncode.h>
+#include "../../Logger/FuncionesLoggers.h"
+#include "../EstructurasDatos/Conjuntos/CLASE_AdaptadorFuncionComparadorIgualdad.h"
+    template <size_t CAPACIDAD_MAPA_ARGUMENTOS, size_t CAPACIDAD_MAPA_ENCABEZADOS, size_t CAPACIDAD_URI_COMPLETA>
+    class SolicitudSinPayload {
+        private:
+            NetworkClient &clienteRed;
+            CallbackResultado<HTTPClient, int> *callbackRecepcionRespuesta;
+            
+            const __FlashStringHelper *dominio;
+            uint16_t puertoTCP;
+            const __FlashStringHelper *uriRecurso;
+            bool usarHTTPs;
+            
+            const __FlashStringHelper *metodoHTTP;
+            
+            AdaptadorFuncionComparadorIgualdad<const __FlashStringHelper *> adaptadorIgualdad;
+            ArrayMapa<const __FlashStringHelper *, const __FlashStringHelper *, CAPACIDAD_MAPA_ARGUMENTOS> argumentos;
+            ArrayMapa<const __FlashStringHelper *, const __FlashStringHelper *, CAPACIDAD_MAPA_ENCABEZADOS> encabezados;
+        
+        protected:
+            virtual int enviarSolicitudBajoNivel(HTTPClient &cliente, const char *metodoCopiado) {
+                return cliente.sendRequest(metodoCopiado);
+            }
+        
+        public:
+            SolicitudSinPayload(NetworkClient &clienteRed, CallbackResultado<HTTPClient, int> *callbackRecepcionRespuesta)
+                : clienteRed(clienteRed)
+                , callbackRecepcionRespuesta(callbackRecepcionRespuesta)
+                
+                , adaptadorIgualdad(AdaptadorFuncionComparadorIgualdad<const __FlashStringHelper *>(&iguales))
+                , argumentos(ArrayMapa<const __FlashStringHelper *, const __FlashStringHelper *, CAPACIDAD_MAPA_ARGUMENTOS>(&adaptadorIgualdad))
+                , encabezados(ArrayMapa<const __FlashStringHelper *, const __FlashStringHelper *, CAPACIDAD_MAPA_ARGUMENTOS>(&adaptadorIgualdad))
+            {}
+            
+            void settearDominio(const __FlashStringHelper *ingr) {
+                this -> dominio = ingr;
+            }
+            
+            void settearPuerto(const uint16_t ingr) {
+                this -> puertoTCP = ingr;
+            }
+            
+            void settearURI(const __FlashStringHelper *ingr) {
+                this -> uriRecurso = ingr;
+            }
+            
+            void settearHTTPs(const bool ingr) {
+                this -> usarHTTPs = ingr;
+            }
+            
+            void settearMetodo(const __FlashStringHelper *ingr) {
+                this -> metodoHTTP = ingr;
+            }
+            
+            bool agregarArgumento(const __FlashStringHelper *clave, const __FlashStringHelper *valor) {
+                return (this -> argumentos.agregar(clave, valor));
+            }
+            
+            bool agregarEncabezado(const __FlashStringHelper *clave, const __FlashStringHelper *valor) {
+                return (this -> encabezados.agregar(clave, valor));
+            }
+            
+            bool enviar(void) {
+                HTTPClient cliente;
+                StringEstatica<CAPACIDAD_URI_COMPLETA> uriCompleta;
+                
+                uriCompleta.agregarFinal(this -> uriRecurso);
+                uriCompleta.agregarFinal('?');
+                
+                for (const ElementoMapa<const __FlashStringHelper *, const __FlashStringHelper *> &selec: this -> argumentos) {
+                    uriCompleta.agregarFinal(urlEncode(selec.getClave()));
+                    uriCompleta.agregarFinal('=');
+                    uriCompleta.agregarFinal(urlEncode(selec.getValor()));
+                    
+                    uriCompleta.agregarFinal('&');
+                }
+                
+                for (const ElementoMapa<const __FlashStringHelper *, const __FlashStringHelper *> &selec: this -> encabezados) {
+                    cliente.addHeader(selec.getClave(), selec.getValor());
+                }
+                
+                if (!cliente.begin(this -> clienteRed, this -> dominio, this -> puertoTCP, uriCompleta.getContenidoConstante(), this -> usarHTTPs)) {
+                    FLOGS("ERROR: No se pudo iniciar el cliente HTTP(s)");
+                    return false;
+                }
+                
+                StringEstatica<8> metodoCopiado;
+                AdaptadorStringImpresora adaptadorMetodo(&metodoCopiado);
+                
+                adaptadorMetodo.print(this -> metodoHTTP);
+                int retorno = (this -> enviarSolicitudBajoNivel(cliente, metodoCopiado.getContenidoConstante()));
+                
+                if (retorno < 0) {
+                    CLOG("ERROR: Al enviar la solicitud, se obtuvo el valor", retorno, '=', cliente.errorToString(retorno));
+                    return false;
+                }
+                
+                this -> callbackRecepcionRespuesta -> notificar(cliente, retorno);
+                return true;
+            }
+    };
+#endif
